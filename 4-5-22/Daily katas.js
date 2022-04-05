6b2d81d37bbeb3814c8209769e7c2d49be322923
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-04-05 12:33:11 -0400

    passed vowel remover 8kyu

--- 4-5-22/Daily katas.js
+++ 4-5-22/Daily katas.js
@@ -21,3 +21,25 @@ function mouthSize(animal) {
     if (animal.toLowerCase() === 'alligator') return 'small'
     return 'wide'
   }
+
+//   Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+//   Examples
+//   "hello"     -->  "hll"
+//   "codewars"  -->  "cdwrs"
+//   "goodbye"   -->  "gdby"
+//   "HELLO"     -->  "HELLO"
+//   don't worry about uppercase vowels
+//   y is not considered a vowel for this kata
+
+function shortcut (string) {
+    let arrayOfVowels = ['a', 'e', 'i', 'o', 'u']
+    let stringArray = string.split('')
+    for (let i = 0; i < stringArray.length; i++) {
+        if (arrayOfVowels.includes(stringArray[i])) {
+            stringArray.splice(i, 1)
+            i--
+        }
+    }
+    return stringArray.join('')
+  }
