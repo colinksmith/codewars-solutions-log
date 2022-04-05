cb5fb977c69154422de04c281e1d818daed5aaa5
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-04-05 12:00:03 -0400

    passed the wide-mouthed frog 8kyu

--- 4-5-22/The Feast of Many Beasts.js
+++ 4-5-22/The Feast of Many Beasts.js
@@ -8,3 +8,16 @@ function feast(beast, dish) {
     if (beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]) return true
     return false
 }
+
+// The wide-mouth frog is particularly interested in the eating habits of other creatures.
+
+// He just can't stop asking the creatures he encounters what they like to eat. But, then he meets the alligator who just LOVES to eat wide-mouthed frogs!
+
+// When he meets the alligator, it then makes a tiny mouth.
+
+// Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide
+
+function mouthSize(animal) {
+    if (animal.toLowerCase() === 'alligator') return 'small'
+    return 'wide'
+  }
