3c9d24095d71281cb41cf5c4a92a785d5101d615
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-04-06 14:51:17 -0400

    passed Name Shuffler 8kyu

--- 4-6-22/Daily katas.js
+++ 4-6-22/Daily katas.js
@@ -77,3 +77,12 @@ function getDrinkByProfession(param){
         default: return "Beer"
     }
   }
+
+//   Name Shuffler
+//   Write a function that returns a string in which firstname is swapped with last name.
+
+//   nameShuffler('john McClane'); => "McClane john"
+
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+  }
