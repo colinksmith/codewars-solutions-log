1e8c22bc711971ce403ea1b0b91dc20b0b59c673
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-04-06 14:49:53 -0400

    passed L1: Bartender, drinks! 2 8kyu

--- 4-6-22/Daily katas.js
+++ 4-6-22/Daily katas.js
@@ -38,3 +38,42 @@ function apple(x){
     return 'Help yourself to a honeycomb Yorkie for the glovebox.'
 }
 
+// L1: Bartender, drinks!
+// Complete the function that receives as input a string, and produces outputs according to the following table:
+
+// Input	Output
+// "Jabroni"	"Patron Tequila"
+// "School Counselor"	"Anything with Alcohol"
+// "Programmer"	"Hipster Craft Beer"
+// "Bike Gang Member"	"Moonshine"
+// "Politician"	"Your tax dollars"
+// "Rapper"	"Cristal"
+// anything else	"Beer"
+// Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
+
+// Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
+
+function getDrinkByProfession(param){
+    param = param.toLowerCase()
+    switch(param) {
+        case "jabroni": 
+          return "Patron Tequila"
+          break
+        case "school counselor": 
+          return "Anything with Alcohol"
+          break
+        case "programmer": 
+          return "Hipster Craft Beer"
+          break
+        case "bike gang member": 
+          return "Moonshine"
+          break
+        case "politician": 
+          return "Your tax dollars"
+          break
+        case "rapper": 
+          return "Cristal"
+          break
+        default: return "Beer"
+    }
+  }
