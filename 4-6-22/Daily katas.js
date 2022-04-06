d0c55f7bd14438272cd88d377a080e94b228b321
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-04-06 14:53:24 -0400

    passed Palindrome strings 8kyu

--- 4-6-22/Daily katas.js
+++ 4-6-22/Daily katas.js
@@ -86,3 +86,21 @@ function getDrinkByProfession(param){
 function nameShuffler(str){
     return str.split(' ').reverse().join(' ')
   }
+
+//   Palindrome strings
+//   A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. This includes capital letters, punctuation, and word dividers.
+  
+//   Implement a function that checks if something is a palindrome. If the input is a number, convert it to string first.
+  
+//   Examples
+//   isPalindrome("anna")   ==> true
+//   isPalindrome("walter") ==> false
+//   isPalindrome(12321)    ==> true
+//   isPalindrome(123456)   ==> false
+
+function isPalindrome(line) {
+    line = String(line)
+    let reverse = line.split('').reverse().join('')
+    if (line === reverse) return true
+    return false
+}
