c1c775d59828615d27a278e1afd28783fe4ffc99
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-04-06 15:02:55 -0400

    passed Regex count lowercase letters 8kyu

--- 4-6-22/Daily katas.js
+++ 4-6-22/Daily katas.js
@@ -104,3 +104,30 @@ function isPalindrome(line) {
     if (line === reverse) return true
     return false
 }
+
+// Regex count lowercase letters
+// Your task is simply to count the total number of lowercase letters in a string.
+
+// Examples
+// lowercaseCount("abc"); ===> 3
+
+// lowercaseCount("abcABC123"); ===> 3
+
+// lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 3
+
+// lowercaseCount(""); ===> 0;
+
+// lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 0
+
+// lowercaseCount("abcdefghijklmnopqrstuvwxyz
+
+function lowercaseCount(str){
+    let arrayOfLetters = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    let count = 0
+    for (i = 0; i < str.length; i++) {
+        if (arrayOfLetters.includes(str[i])) {
+            count++
+        }
+    }
+    return count
+}
