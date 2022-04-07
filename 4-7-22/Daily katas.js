6d5afde8651abd2d53343e1f7b84c0fabf5b0b81
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-04-07 10:32:30 -0400

    passed String cleaning 8kyu

---
+++ 4-7-22/Daily katas.js
@@ -0,0 +1,13 @@
+// String cleaning
+// Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
+
+// Examples (input -> output)
+// '! !'                 -> '! !'
+// '123456789'           -> ''
+// 'This looks5 grea8t!' -> 'This looks great!'
+// Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+
+function stringClean(s){
+  let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+  return s.split('').filter(character => numbers.includes(character) === false).join('')
+}
