355dd70ec0fb58249b63e2222df168895ab99844
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-04-07 11:09:33 -0400

    passed repeatIt 8kyu

--- 4-7-22/Daily katas.js
+++ 4-7-22/Daily katas.js
@@ -11,3 +11,22 @@ function stringClean(s){
   let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
   return s.split('').filter(character => numbers.includes(character) === false).join('')
 }
+
+// repeatIt
+// Create a function that takes a string and an integer (n).
+
+// The function should return a string that repeats the input string n number of times.
+
+// If anything other than a string is passed in you should return "Not a string"
+
+// Example
+// "Hi", 2 --> "HiHi"
+// 1234, 5 --> "Not a string"
+
+var repeatIt = function(str, n) {
+  if (typeof str !== 'string') {
+    return 'Not a string'
+  } else {
+    return str.repeat(n)
+  }
+}
