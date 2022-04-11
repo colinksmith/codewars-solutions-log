84a8551f20a0eca0037f6258f8f1c09e0d16129b
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-04-11 17:24:18 -0400

    passed Define a card suit 8kyu

---
+++ 4-11-22/Daily katas.js
@@ -0,0 +1,20 @@
+// Define a card suit
+// You get any card as an argument. Your task is to return the suit of this card (in lowercase).
+
+// Our deck (is preloaded):
+
+// deck = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+//         '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+//         '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+//         '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠'];
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+function defineSuit(card) {
+    if (card.includes('♣')) return 'clubs'
+    if (card.includes('♦')) return 'diamonds'
+    if (card.includes('♥')) return 'hearts'
+    if (card.includes('♠')) return 'spades'
+  }
