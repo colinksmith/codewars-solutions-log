56befa74c6afd9d05d11852c05502a5b1115b853
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-03 19:35:11 -0400

    passed String Templates - Bug Fixing #5 8kyu

--- 5-3-22/Daily katas.js
+++ 5-3-22/Daily katas.js
@@ -4,3 +4,15 @@
 function buildString(...template){
     return `I like ${template.join(', ')}!`;
   }
+
+//   Simple validation of a username with regex
+//   Write a simple regex to validate a username. Allowed characters are:
+
+// lowercase letters,
+// numbers,
+// underscore
+// Length should be between 4 and 16 characters (both included).
+
+function validateUsr(username) {
+    return /^[0-9a-z_]{4,16}$/.test(username)
+  }
