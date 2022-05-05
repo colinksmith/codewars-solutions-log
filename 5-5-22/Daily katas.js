8c1b15ca873317300f153afbd1be3dc05104fa4d
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-05 16:23:02 -0400

    passed Grasshopper - Personalized Message 8kyu

--- 5-5-22/Daily katas.js
+++ 5-5-22/Daily katas.js
@@ -3,4 +3,19 @@
 
 function doubleInteger(i) {
     return i * 2;
-  }
+}
+
+// Grasshopper - Personalized Message
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+function greet (name, owner) {
+    if (name === owner) {
+        return "Hello boss"
+    }
+    return "Hello guest"
+}
