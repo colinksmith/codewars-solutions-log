fe3e34fd56a1df3c1141bb9feb4a37ca7f7d45c2
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-06 11:38:12 -0400

    passed Thinkful - Logic Drills: Traffic light 8kyu

--- 5-6-22/Daily katas.js
+++ 5-6-22/Daily katas.js
@@ -49,3 +49,18 @@ function getPlanetName(id){
     
     return name;
 }
+
+// Thinkful - Logic Drills: Traffic light
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+// For example, update_light('green') should return 'yellow'.
+
+// Note: Use "" instead of '' in C++.
+
+function updateLight(current) {
+    let lightArray = ['green', 'yellow', 'red']
+    let number = lightArray.indexOf(current)
+    return lightArray[number + 1] || lightArray[0]
+}
