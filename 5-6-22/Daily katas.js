3c63fb87355baf05606cb41b95a1a74f63f9f3aa
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-06 10:48:53 -0400

    passed Double Char 8kyu

---
+++ 5-6-22/Daily katas.js
@@ -0,0 +1,14 @@
+// Double Char
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+// Good Luck!
+
+function doubleChar(str) {
+    return str.split('').map(x => x.repeat(2)).join('')
+}
+
+
