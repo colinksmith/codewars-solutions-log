ae96ec011b3fef84969f928cda98e0edabd753e9
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-08 11:22:28 -0400

    passed Remove exclamation marks 8kyu

---
+++ 5-8-22/Daily katas.js
@@ -0,0 +1,6 @@
+// Remove exclamation marks
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+    return s.split('!').join('')
+}
