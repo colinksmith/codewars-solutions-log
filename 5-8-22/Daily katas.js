f36f45e7d8951336b28172a24a358d4199447163
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-08 16:38:15 -0400

    passed Twice as old 8kyu

--- 5-8-22/Daily katas.js
+++ 5-8-22/Daily katas.js
@@ -4,3 +4,15 @@
 function removeExclamationMarks(s) {
     return s.split('!').join('')
 }
+
+// Twice as old
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    let ageDifference = dadYearsOld - sonYearsOld
+    return Math.abs(ageDifference - sonYearsOld)
+}
