bcae27f2591985a0321fe67f2c3f307270aca1f3
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-09 15:46:57 -0400

    passed Sammest unused ID 8kyu

--- 5-9-22/Daily katas.js
+++ 5-9-22/Daily katas.js
@@ -128,3 +128,28 @@ function getSize(width, height, depth){
     let area = width * height * 2 + height * depth * 2 + depth * width * 2
     return [area, volume]
 }
+
+// Smallest unused ID
+// Hey awesome programmer!
+
+// You've got much data to manage and of course you use zero-based and non-negative ID's to make each data item unique!
+
+// Therefore you need a method, which returns the smallest unused ID for your next new data item...
+
+// Note: The given array of used IDs may be unsorted. For test reasons there may be duplicate IDs, but you don't have to find or remove them!
+
+// Go on and code some pure awesomeness!
+
+function nextId(ids){
+    let id = -1
+    let i = 0
+    while (id === -1) {
+        if (ids.includes(i)){
+            i++
+            continue
+        }
+        id = i
+        i++
+    }
+    return id
+}
