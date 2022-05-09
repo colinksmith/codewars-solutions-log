b46bbeac3168cae80e7afaa737aea7e220f3fd57
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-09 15:55:57 -0400

    passed Formatting decimal places #0 8kyu

--- 5-9-22/Daily katas.js
+++ 5-9-22/Daily katas.js
@@ -179,3 +179,13 @@ function nextId(ids){
 function remainder(n, m){
     return n < m ? m % n : n % m    
 }
+
+// Formatting decimal places #0
+// Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+// Example:    
+// 5.5589 is rounded 5.56   
+// 3.3424 is rounded 3.34
+function twoDecimalPlaces(n) {
+    return +n.toFixed(2)
+}
