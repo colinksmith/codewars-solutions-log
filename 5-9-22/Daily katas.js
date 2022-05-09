f697e6cdddf4d3df4922291c86433f2d10b50a9d
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-09 15:32:00 -0400

    passed Surface Area and Volume of a Box 8kyu

--- 5-9-22/Daily katas.js
+++ 5-9-22/Daily katas.js
@@ -119,3 +119,12 @@ function dutyFree(normPrice, discount, hol){
 function toBinary(n){
     return Number(n.toString(2))
 }
+
+// Surface Area and Volume of a Box
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
+function getSize(width, height, depth){
+    let volume = width * height * depth
+    let area = width * height * 2 + height * depth * 2 + depth * width * 2
+    return [area, volume]
+}
