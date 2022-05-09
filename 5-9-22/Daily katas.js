4c3dd11d26a20b2afafb6ec1ef619a65a48fd957
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-09 15:28:24 -0400

    passed Convert to Binary 8kyu

--- 5-9-22/Daily katas.js
+++ 5-9-22/Daily katas.js
@@ -102,3 +102,20 @@ function between(a, b) {
 function dutyFree(normPrice, discount, hol){
     return Math.floor(hol / (normPrice * discount / 100))
 }
+
+// Convert to Binary
+// Task Overview
+// Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+
+// to_binary(1)  /* should return 1 */
+// to_binary(5)  /* should return 101 */
+// to_binary(11) /* should return 1011 */
+// Example:
+
+// toBinary(1)  /* should return 1 */
+// toBinary(5)  /* should return 101 */
+// toBinary(11) /* should return 1011 */
+
+function toBinary(n){
+    return Number(n.toString(2))
+}
