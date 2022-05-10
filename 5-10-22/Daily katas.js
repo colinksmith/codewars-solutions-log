2c0f9628a8a6077d03aed4c0a152a256f6716983
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-10 11:19:35 -0400

    passed Evil or Odious 8kyu

--- 5-10-22/Daily katas.js
+++ 5-10-22/Daily katas.js
@@ -21,3 +21,29 @@ function pointsPer48(ppg, mpg) {
     let output = ppg / fraction
     return +output.toFixed(1)
 }
+
+// Evil or Odious
+// The number n is Evil if it has an even number of 1's in its binary representation.
+// The first few Evil numbers: 3, 5, 6, 9, 10, 12, 15, 17, 18, 20
+
+// The number n is Odious if it has an odd number of 1's in its binary representation.
+// The first few Odious numbers: 1, 2, 4, 7, 8, 11, 13, 14, 16, 19
+
+// You have to write a function that determine if a number is Evil of Odious, function should return "It's Evil!" in case of evil number and "It's Odious!" in case of odious number.
+
+// good luck :)
+
+function evil(n) {
+    let binary = n.toString(2)
+    let accumulator = 0
+    for (let i = 0; i < binary.length; i++) {
+        if (binary[i] === '1'){
+            accumulator++
+        }
+    }
+    if (accumulator % 2 === 0) {
+        return "It's Evil!"
+    } else {
+        return "It's Odious!"
+    }
+}
