2a75c554626064398529a45153d0f0a2911ae883
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-10 11:57:29 -0400

    passed Geometry Basics: Distance between points in 2D 8kyu

--- 5-10-22/Daily katas.js
+++ 5-10-22/Daily katas.js
@@ -88,3 +88,16 @@ function fuelPrice(litres, pricePerLitre) {
     }
     return Number(cost.toFixed(2))
 }
+
+// Geometry Basics: Distance between points in 2D
+// This series of katas will introduce you to basics of doing geometry with computers.
+
+// Point objects have x and y attributes (X and Y in C#) attributes.
+
+// Write a function calculating distance between Point a and Point b.
+
+// Tests round answers to 6 decimal places.
+
+function distanceBetweenPoints(a, b) {
+    return Math.sqrt((b.x - a.x) ** 2 + (b.y - a.y) ** 2)
+}
