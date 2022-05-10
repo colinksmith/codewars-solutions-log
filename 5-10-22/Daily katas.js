ccd783d84fbecd5006049041d599ff5fad592d2e
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-10 11:33:15 -0400

    passed Fuel Calculator 8kyu

--- 5-10-22/Daily katas.js
+++ 5-10-22/Daily katas.js
@@ -60,3 +60,31 @@ function evil(n) {
 function ifChuckSaysSo(){
     return 1 < 0
 }
+
+// Fuel Calculator
+// In this kata you will have to write a function that takes litres and pricePerLitre (in dollar) as arguments.
+
+// Purchases of 2 or more litres get a discount of 5 cents per litre, purchases of 4 or more litres get a discount of 10 cents per litre, and so on every two litres, up to a maximum discount of 25 cents per litre. But total discount per litre cannot be more than 25 cents. Return the total cost rounded to 2 decimal places. Also you can guess that there will not be negative or non-numeric inputs.
+
+// Good Luck!
+
+// Note
+// 1 Dollar = 100 Cents
+
+function fuelPrice(litres, pricePerLitre) {
+    cost = 0
+    if (litres < 2) {
+        cost = pricePerLitre * litres
+    } else if (litres < 4) {
+        cost = (pricePerLitre - .05) * litres
+    } else if (litres < 6) {
+        cost = (pricePerLitre - .1) * litres
+    } else if (litres < 8) {
+        cost = (pricePerLitre - .15) * litres
+    } else if (litres < 10) {
+        cost = (pricePerLitre - .20) * litres
+    } else if (litres >= 10) {
+        cost = (pricePerLitre - .25) * litres
+    }
+    return Number(cost.toFixed(2))
+}
