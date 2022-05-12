923e366bdc247294d3bb134bc658f7da23de1df1
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-12 16:13:01 -0400

    Passed Grasshopper - Grade book 8kyu

---
+++ 05-12-22/Daily katas.js
@@ -0,0 +1,26 @@
+// Grasshopper - Grade book
+// Grade book
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+function getGrade (s1, s2, s3) {
+    value = (s1 + s2 + s3) / 3
+    if (value >= 90) {
+        return 'A'
+    } else if (value >= 80) {
+        return 'B'
+    } else if (value >= 70) {
+        return 'C'
+    } else if (value >= 60) {
+        return 'D'
+    } else {
+        return 'F'
+    }
+}
