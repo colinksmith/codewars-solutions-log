b6edfc4b30b6f8f5155d6b73c776421fcd2a5945
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-13 12:13:56 -0400

    passed Potenciation 8kyu

--- 05-13-22/Daily katas.js
+++ 05-13-22/Daily katas.js
@@ -113,3 +113,18 @@ function barTriang(p1, p2, p3){
 function circleArea(circle){
     return circle.radius ** 2 * Math.PI
 }
+
+// Potenciation
+// The function takes in 2 inputs x and y, and should return x to the power of y
+
+// Simple, right? But you're NOT allowed to use Math.pow();
+
+// Obs: x and y will be naturals, so DON'T take fractions into consideration;
+
+// Note : zero to the power of zero equals one in this kata
+
+// Great coding <3
+
+function power(x,y){
+    return x ** y
+}
