9041a90c9d3ce9d329f975ff820b9f4160cb4c11
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-13 12:10:00 -0400

    passed Geometry Basics: Circle Area in 2D 8kyu

--- 05-13-22/Daily katas.js
+++ 05-13-22/Daily katas.js
@@ -85,3 +85,31 @@ function barTriang(p1, p2, p3){
     y0 = (p1[1] + p2[1] + p3[1]) / 3
     return [+(x0.toFixed(4)), +(y0.toFixed(4))]
 }
+
+// Geometry Basics: Circle Area in 2D
+// This series of katas will introduce you to basics of doing geometry with computers.
+
+// Write the function circleArea/CircleArea which takes in a Circle object and calculates the area of that circle.
+// The Circle class can be seen below:
+
+// // Represents a Circle where center is a Point and radius is a Number
+// class Circle {
+//   constructor(center, radius) { 
+//     this.center = center; 
+//     this.radius = radius;
+//   }
+// }
+// And the Point class can be seen below:
+
+// // Represents a Point where x and y are Numbers
+// class Point {
+//   constructor (x,y) { 
+//     this.x = x;
+//     this.y = y; 
+//   }
+// }
+// Tests round answers to 6 decimal places.
+
+function circleArea(circle){
+    return circle.radius ** 2 * Math.PI
+}
