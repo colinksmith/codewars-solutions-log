bf187129198b88f981bc6c49b11f2b47d0b45136
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-13 13:51:48 -0400

    passed Blood-Alcohol Content 8kyu

--- 05-13-22/Daily katas.js
+++ 05-13-22/Daily katas.js
@@ -185,3 +185,38 @@ Math.roundTo = function (number, precision) {
 function aspectRatio(x,y){
     return [(Math.ceil(y*16/9)), y]
 }
+
+// Blood-Alcohol Content
+// Bob drinks too much, and he gets in trouble for it a lot. He drinks so much, in fact, that he has broken the local law enforcement's breathalizer with his alcoholic breath! Bob feels simply aweful, so he wants to make up for it by creating a function that will calculate his blood-alcohol level for them. Unfortunately, Bob has gotten too inebriated to do any programming today, so he needs your help!
+
+// He did manage to research the formula for blood-alcohol content before getting too drunk, which he describes below.
+
+// BAC calculator Formula:
+
+// BAC% = (A × 5.14 / W × r) - .015 × H 
+
+// A: Total alcohol consumed, in ounces (oz)
+// W: Body weight, in pounds (lbs)
+// r: The alcohol distribution ratio, 0.73 for man, and 0.66 for women
+// H: Time passed since drinking, in hours
+// Source:
+
+// http://www.endmemo.com/medical/bac.php
+
+// Alcohol consumed will be passed as a drinks object with two properties: ounces (the total volume of beverage consumed in ounces), and abv (the % of alcohol by volume of the beverage as a floating point number--such as 0.05 for 5% abv beer or 0.4 for 40% abv whisky). For simplicity, Bob assures us that he drinks the same kind of beverage each time he drinks.
+
+// The gender will be passed as a string, either "male" or "female".
+
+// Output must be returned as a number data-type, greater than or equal to 0, with up to 4 decimal places. No error handling will be required.
+
+// Using these parameters, create the function that will calculate Bob's and other partier's BAC.
+
+function bloodAlcoholContent(drinks, weight, sex, time){
+    if (sex === 'male') {
+        sexConstant = 0.73
+    } else {
+        sexConstant = 0.66
+    }
+    bac = ((drinks.ounces * drinks.abv) * 5.14 / weight * sexConstant) - (.015 * time)
+    return +bac.toFixed(4);
+}
