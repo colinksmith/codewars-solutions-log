59e490d683c34d55a5b65509c9b7053756e08f02
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-14 11:20:53 -0400

    passed Character Frequency 8kyu

--- 05-14-22/Daily katas.js
+++ 05-14-22/Daily katas.js
@@ -83,3 +83,28 @@ function correctPolishLetters (string) {
 function joinStrings(string1, string2){
     return `${string1} ${string2}`
 }
+
+// Character Frequency
+// Description
+// Welcome, Warrior! In this kata, you will get a message and you will need to get the frequency of each and every character!
+
+// Explanation
+// Your function will be called char_freq/charFreq/CharFreq and you will get passed a string, you will then return a dictionary (object in JavaScript) with as keys the characters, and as values how many times that character is in the string. You can assume you will be given valid input.
+
+// Example
+// charFreq("I like cats") // Returns {'a': 1, ' ': 2, 'c': 1, 'e': 1, 'I': 1, 'k': 1, 'l': 1, 'i': 1, 's': 1, 't': 1}
+
+function charFreq(message) {
+    return message
+        .split('')
+        .reduce((accumulatorObj, letter)  => {
+            if (letter in accumulatorObj) {
+                accumulatorObj[letter]++
+                return accumulatorObj
+            } else {
+                accumulatorObj[letter] = 1
+                return accumulatorObj
+            }
+        
+        }, {})
+}
