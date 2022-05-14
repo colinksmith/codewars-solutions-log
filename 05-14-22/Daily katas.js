9b452b24b97db02a449a8eff55f032afd3d22455
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-14 12:30:18 -0400

    passed Crash Override 8kyu

--- 05-14-22/Daily katas.js
+++ 05-14-22/Daily katas.js
@@ -117,3 +117,26 @@ function charFreq(message) {
 function add(a, b){
 	return a == b ? true : false
 }
+
+// Crash Override
+// Every budding hacker needs an alias! The Phantom Phreak, Acid Burn, Zero Cool and Crash Override are some notable examples from the film Hackers.
+
+// Your task is to create a function that, given a proper first and last name, will return the correct alias.
+
+// Two objects that return a one word name in response to the first letter of the first name and one for the first letter of the surname are already given.
+
+// If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+
+// Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+
+// var firstName = {A: 'Alpha', B: 'Beta', C: 'Cache' ...}
+// var surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst' ...}
+
+// aliasGen('Larry', 'Brentwood') === 'Logic Bomb'
+// aliasGen('123abc', 'Petrovic') === 'Your name must start with a letter from A - Z.'
+// Happy hacking!
+function aliasGen(first, sur){
+    if (first[0].toUpperCase() in firstName && sur[0].toUpperCase() in surname) {
+        return (firstName[first[0].toUpperCase()] + ' ' + surname[sur[0].toUpperCase()] )
+    } else {return "Your name must start with a letter from A - Z."}
+}
