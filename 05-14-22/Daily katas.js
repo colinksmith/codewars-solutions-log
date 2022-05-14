e3361da02032b78f0965923d7c8be0feb2688967
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-14 15:09:15 -0400

    passed Simple Change Machine 8kyu

--- 05-14-22/Daily katas.js
+++ 05-14-22/Daily katas.js
@@ -178,3 +178,41 @@ function countWords(str) {
     if (inWord === true) count++ 
     return count
 }
+
+// Simple Change Machine
+// Your task is to create a change machine.
+
+// The machine accepts a range of specified coins and notes, it returns change in 20p and 10p coins in the minimum amount of pieces. A 50p for example would return two 20p pieces and one 10p piece. The machine will always try and return change, if you input a 20p for example it will return "10p 10p".
+
+// The machine accepts these coins and notes: £5, £2, £1, 50p, 20p. Any coins and notes which are not accepted by the machine will be returned. If you were to put a £20 note into the machine for example, it would be returned to you and not broken into change.
+
+// This change machine is programmed to accept and distribute strings rather than numbers. The change will be returned as one string with the change separated by single spaces & no commas. The values of the string will be descending.
+
+function changeMe(moneyIn){
+    keyObj = {
+        '£5': 500,
+        '£2': 200,
+        '£1': 100,
+        '50p': 50,
+        '20p': 20,
+        '10p': 10,
+    }
+    if (!(moneyIn in keyObj)) {
+        return moneyIn
+    }
+    let convertedMoney = keyObj[moneyIn]
+    let outputArray = []
+    if (convertedMoney === 20) {
+        return '10p 10p'
+    }
+    while (convertedMoney !== 0) {
+        if (convertedMoney > 10) {
+            outputArray.push('20p')
+            convertedMoney -= 20
+        } else {
+            outputArray.push('10p')
+            convertedMoney -= 10
+        }
+    }
+    return outputArray.join(' ')
+}
