f63d1d5ed91d8a7d9aad6c064c44723caa7cb9ce
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-14 10:19:39 -0400

    passed Template Strings 8kyu

--- 05-14-22/Daily katas.js
+++ 05-14-22/Daily katas.js
@@ -13,3 +13,18 @@ function validateCode (code) {
 String.prototype.digit = function() {
     return this <= '9' && this >= '0' && this.length == 1
 };
+
+// Template Strings
+// Template Strings
+// Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+// Task
+// Your task is to return the correct string using the Template String Feature.
+// Input
+// Two Strings, no validation is needed.
+// Output
+// You must output a string containing the two strings with the word ```' are '```
+// Reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/template_strings
+
+var TempleStrings = function(obj, feature) {
+    return `${obj} are ${feature}`
+}
