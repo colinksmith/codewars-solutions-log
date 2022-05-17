46344bd49c31c72591de1a650178cfeeb315ae63
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-17 13:17:04 -0400

    passed Is it even? 8kyu

--- 05-17-22/Daily katas.js
+++ 05-17-22/Daily katas.js
@@ -41,3 +41,18 @@ const quarterOf = (month) => {
 function getAge(inputString){
     return Number(inputString[0])
 }
+
+// Is it even?
+// In this Kata we are passing a number (n) into a function.
+
+// Your code will determine if the number passed is even (or not).
+
+// The function needs to return either a true or false.
+
+// Numbers may be positive or negative, integers or floats.
+
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+function testEven(n) {
+    return n % 2 === 0 ? true : false
+}
