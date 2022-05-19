48dd5dafa263784e12f44033f228ada69b98f91b
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-19 18:13:05 -0400

    passed Third Angle of a Triangle 8kyu

---
+++ 05-19-22/Daily katas.js
@@ -0,0 +1,12 @@
+// Third Angle of a Triangle
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// https://en.wikipedia.org/wiki/Triangle
+
+function otherAngle(a, b) {
+    return 180 - a - b
+}
