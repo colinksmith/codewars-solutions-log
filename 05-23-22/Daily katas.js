2402b6be8f20254cc424ac733c4e0c818e9560c9
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-23 18:58:44 -0400

    passed Squash the bugs 8kyu

---
+++ 05-23-22/Daily katas.js
@@ -0,0 +1,17 @@
+// Squash the bugs
+// Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. Output should be the length of the longest word, as a number.
+
+// There will only be one 'longest' word.
+
+function findLongest(str) {
+  
+    var spl = str.split(" ");
+    var longest = 0
+    
+    for (var i = 0; i < spl.length; i++) {
+      if (spl[i].length > longest) {
+        longest = spl[i].length
+      }
+      }
+      return longest
+}
