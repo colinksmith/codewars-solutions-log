c483cdf43c59465bc28151dde0d49b2c3135aaf4
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-05-27 08:37:08 -0400

    passed Cat years, Dog years 8kyu

---
+++ 05-27-22/Daily katas.js
@@ -0,0 +1,40 @@
+// Cat years, Dog years
+// Kata Task
+// I have a cat and a dog.
+
+// I got them at the same time as kitten/puppy. That was humanYears years ago.
+
+// Return their respective ages now as [humanYears,catYears,dogYears]
+
+// NOTES:
+
+// humanYears >= 1
+// humanYears are whole numbers only
+// Cat Years
+// 15 cat years for first year
+// +9 cat years for second year
+// +4 cat years for each year after that
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each year after that
+// References
+
+// http://www.catster.com/cats-101/calculate-cat-age-in-cat-years
+// http://www.slate.com/articles/news_and_politics/explainer/2009/05/a_dogs_life.html
+
+var humanYearsCatYearsDogYears = function(humanYears) {
+    let catYears, dogYears
+    if (humanYears === 1){
+        catYears = 15
+        dogYears = 15
+    } else if (humanYears === 2){
+        catYears = 24
+        dogYears = 24
+    } else {
+        catYears = 24 + (humanYears - 2) * 4
+        dogYears = 24 + (humanYears - 2) * 5
+    }
+    return [humanYears,catYears,dogYears];
+}
+  
