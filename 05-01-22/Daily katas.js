db5920a2d7360132a94558f6e00b85e44c899677
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-06-01 17:02:15 -0400

    passed Grasshopper - Messi goals function 8kyu

---
+++ 05-01-22/Daily katas.js
@@ -0,0 +1,17 @@
+// Grasshopper - Messi goals function
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+
+// 5, 10, 2  -->  17
+function goals (a, b, c) {
+    return a + b + c
+}
