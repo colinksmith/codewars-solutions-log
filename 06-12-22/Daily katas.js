d0f6b870c763f442769b5cb2a5e82c30619fbb20
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-06-12 10:24:23 -0400

    passed Grasshopper - Terminal game combat function .8kyu

--- 06-12-22/Daily katas.js
+++ 06-12-22/Daily katas.js
@@ -178,3 +178,11 @@ function findMultiples(integer, limit) {
     return acc
 }
   
+
+// Grasshopper - Terminal game combat function
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+function combat(health, damage) {
+    return health - damage < 0 ? 0 :
+    health - damage
+}
