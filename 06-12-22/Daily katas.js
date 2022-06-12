0c4717c42b63cb35925d166da3868caf90a1a85a
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-06-12 10:50:32 -0400

    passed Lario and Muigi Pipe Problem .8kyu

--- 06-12-22/Daily katas.js
+++ 06-12-22/Daily katas.js
@@ -186,3 +186,31 @@ function combat(health, damage) {
     return health - damage < 0 ? 0 :
     health - damage
 }
+
+// Lario and Muigi Pipe Problem
+// Issue
+// Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+// The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+// Pipes list is correct when each pipe after the first index is greater (+1) than the previous one, and that there is no duplicates.
+
+// Task
+// Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+// Example
+// Input: 1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+
+function pipeFix(numbers){
+    const start = numbers.reduce((prev, curr) =>{
+        return prev < curr ? prev : curr
+    })
+    const end = numbers.reduce((prev, curr) =>{
+        return prev > curr ? prev : curr
+    })
+    const result = []
+    for (let i = start; i <= end; i++){
+        result.push(i)
+    }
+    return result
+}
