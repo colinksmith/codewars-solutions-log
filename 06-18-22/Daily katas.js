bdfab25030bf5ae9c4ba1a52622d15aef6ea208b
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-06-19 17:02:54 -0400

    passed Grasshopper - Basic Function Fixer 8kyu

--- 06-18-22/Daily katas.js
+++ 06-18-22/Daily katas.js
@@ -27,3 +27,15 @@ const flip=(d, a)=>{
         return a.sort((a, b) => a - b)
     }
 }
+
+// Grasshopper - Basic Function Fixer
+// DESCRIPTION:
+// Fix the function
+// I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+// Can you help me fix the function?
+
+function addFive(num) {
+    var total = num + 5
+    return total
+}
