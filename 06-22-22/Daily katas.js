90c65ab4204541ef7b56f79a96efbd28e5162859
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-06-22 17:35:15 -0400

    passed The 'if' function 8kyu

--- 06-22-22/Daily katas.js
+++ 06-22-22/Daily katas.js
@@ -6,3 +6,20 @@
 function distinct(a) {
     return [...new Set(a)]
 }
+
+// The 'if' function
+// Create a function called _if which takes 3 arguments: a boolean value bool and 2 functions (which do not take any parameters): func1 and func2
+
+// When bool is truth-ish, func1 should be called, otherwise call the func2.
+
+// Example:
+// _if(true, function(){console.log("True")}, function(){console.log("false")})
+// // Logs 'True' to the console.
+
+function _if(bool, func1, func2) {
+    if (bool){
+        func1()
+    } else {
+        func2()
+    }
+}
