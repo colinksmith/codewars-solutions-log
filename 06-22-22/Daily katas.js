83d5b3fa5359a957880f273ecbf9f2bcd584c096
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-06-22 18:38:03 -0400

    passed Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence 8kyu

--- 06-22-22/Daily katas.js
+++ 06-22-22/Daily katas.js
@@ -67,3 +67,26 @@ Sleigh.prototype.authenticate = function(name, password) {
   }
   return false
 };
+
+// Exclamation marks series #11: Replace all vowel to exclamation mark in the sentence
+// Description:
+// Replace all vowel to exclamation mark in the sentence. aeiouAEIOU is vowel.
+
+// Examples
+// replace("Hi!") === "H!!"
+// replace("!Hi! Hi!") === "!H!! H!!"
+// replace("aeiou") === "!!!!!"
+// replace("ABCDE") === "!BCD!"
+
+function replace(s){
+    objKey = {
+        a: '!',
+        e: '!',
+        i: '!',
+        o: '!',
+        u: '!',
+    }
+    return s.split('')
+     .map(x => x.toLowerCase() in objKey ? '!' : x)
+     .join('')
+}
