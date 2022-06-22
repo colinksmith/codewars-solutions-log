7ea7b6743f64ae68cc65d5d07801e50761b16c74
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-06-22 17:23:49 -0400

    passed Remove duplicates from list 8kyu

---
+++ 06-22-22/Daily katas.js
@@ -0,0 +1,8 @@
+// Remove duplicates from list
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+function distinct(a) {
+    return [...new Set(a)]
+}
