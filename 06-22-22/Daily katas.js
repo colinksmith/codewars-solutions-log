0c0844b4a085207f7e55cb8389f4aa8c80dc13a2
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-06-22 17:44:50 -0400

    passed Exclamation marks series #1: Remove an exclamation mark from the end of string 8kyu

--- 06-22-22/Daily katas.js
+++ 06-22-22/Daily katas.js
@@ -23,3 +23,24 @@ function _if(bool, func1, func2) {
         func2()
     }
 }
+
+// Exclamation marks series #1: Remove an exclamation mark from the end of string
+// Description:
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi!!"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+// Note
+// Please don't post issue about difficulty or duplicate.
+
+function remove (string) {
+    if (string[string.length - 1] === '!'){
+        return string.slice(0, -1)
+    }
+    return string
+}
