39cae6c935a9f462ded6376419c09a26120df5b7
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-06-23 15:00:15 -0400

    passed Kata Example Twist 8kyu

--- 6-23-22/daily-kata.js
+++ 6-23-22/daily-kata.js
@@ -10,3 +10,13 @@
 function usdcny(usd) {
     return `${(usd * 6.75).toFixed(2)} Chinese Yuan`
   }
+
+//Kata Example Twist
+//
+//This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata). 
+//Add the value "codewars" to the array websites/Websites 1,000 times.
+
+var websites = []
+for (let i = 0; i < 1000; i++) {
+    websites.push('codewars')
+}
