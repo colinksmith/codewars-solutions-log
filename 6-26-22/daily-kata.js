ad2369e4a2377ab73c740acebdfbd01ddb71cd5e
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-06-26 15:26:32 -0400

    passed Fix your code before the garden dies!  8kyu

---
+++ 6-26-22/daily-kata.js
@@ -0,0 +1,13 @@
+//Fix your code before the garden dies! 
+//
+//You have an award-winning garden and every day the plants need exactly 40mm of water. You created a great piece of JavaScript to calculate the amount of water your plants will need when you have taken into consideration the amount of rain water that is forecast for the day. Your jealous neighbour hacked your computer and filled your code with bugs. 
+//Your task is to debug the code before your plants die!
+
+function rainAmount(mm){
+    if (mm < 40) {
+         return "You need to give your plant " + (40 - mm) + "mm of water"
+    }
+    else {
+         return "Your plant has had more than enough water for today!"
+    };
+}
