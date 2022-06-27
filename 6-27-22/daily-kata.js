6f33b4df7aadcfcc5195b3d59c756768f4c516d2
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-06-27 15:35:20 -0400

    passed Enumerable Magic #3 - Does My List Include This? 8kyu

---
+++ 6-27-22/daily-kata.js
@@ -0,0 +1,8 @@
+//Enumerable Magic #3 - Does My List Include This?
+//
+//Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+//If you need help, here's some reference material: http://www.rubycuts.com/enum-include
+
+function include(arr, item){
+    return arr.includes(item)
+  }
