86e8a4aead527d634a95fe3f1701e1956c14156d
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-01 16:48:46 -0400

    passed Loading Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right 8kyu

--- 7-1-22/daily-kata.js
+++ 7-1-22/daily-kata.js
@@ -29,3 +29,13 @@ function stairsIn20(s){
     })
     return sum * 20
 }
+
+//Loading Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right
+//
+
+function remove(s,n){
+    for (let i = 0; i < n; i++){
+      s = s.replace('!', '')
+    }
+    return s
+  }
