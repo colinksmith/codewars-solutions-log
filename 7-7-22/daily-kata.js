4c6c6196c76b3d7d0afb89fd48acdd296235b7da
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-07 16:57:53 -0400

    passed Tip Calculator 8kyu

--- 7-7-22/daily-kata.js
+++ 7-7-22/daily-kata.js
@@ -517,3 +517,41 @@ function trueOrFalse(val){
   if (val)    return 'true';             
   else     return 'false';
 }
+
+//Tip Calculator
+//
+//Complete the function, which calculates how much you need to tip based on the total amount of the bill and the service. 
+//You need to consider the following ratings:
+//
+//Terrible: tip 0%
+//Poor: tip 5%
+//Good: tip 10%
+//Great: tip 15%
+//Excellent: tip 20%
+//
+//The rating is case insensitive (so "great" = "GREAT"). If an unrecognised rating is received, then you need to return:
+//
+//"Rating not recognised" in Javascript, Python and Ruby...
+//...or null in Java
+//...or -1 in C#
+//
+//Because you're a nice person, you always round up the tip, regardless of the service.
+
+function calculateTip(amount, rating) {
+  if (typeof amount !=='number') return 'Rating not recognised'
+  if (typeof rating !=='string') return 'Rating not recognised'
+
+  const ratingKey = {
+    terrible: 0,
+    poor: .05,
+    good: .10,
+    great: .15,
+    excellent: .20,
+  }
+  rating = rating.toLowerCase()
+  if (rating in ratingKey) {
+    return Math.ceil(amount * ratingKey[rating])
+  } else {
+  return 'Rating not recognised'
+  }
+}
