5e6e4575ee80ec3566db7b915d3ced7f784850fc
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-07 14:15:46 -0400

    passed Price of Mangoes 8kyu

--- 7-7-22/daily-kata.js
+++ 7-7-22/daily-kata.js
@@ -433,3 +433,16 @@ function sumMul(n,m){
 function checkTheBucket(bucket){
   return bucket.includes('gold')
 }
+
+//Price of Mangoes
+//
+//There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+//Examples
+//mango(3, 3) ==> 6    # 2 mangoes for 3 = 6; +1 mango for free
+//mango(9, 5) ==> 30   # 6 mangoes for 5 = 30; +3 mangoes for free
+//
+
+function mango(quantity, price){
+  let mangoPayCount = Math.ceil(quantity * 2/3)
+  return mangoPayCount * price
+}
