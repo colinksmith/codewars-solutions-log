84c4026362b70d8f3850214a16612e130a781cea
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-07 13:33:57 -0400

    passed Return the day  8kyu

--- 7-7-22/daily-kata.js
+++ 7-7-22/daily-kata.js
@@ -106,3 +106,30 @@ function remove (string) {
     string += '!'
     return string
   }
+
+//Return the day 
+//
+//Complete the function which returns the weekday according to the input number:
+//
+//1 returns "Sunday"
+//2 returns "Monday"
+//3 returns "Tuesday"
+//4 returns "Wednesday"
+//5 returns "Thursday"
+//6 returns "Friday"
+//7 returns "Saturday"
+//Otherwise returns "Wrong, please enter a number between 1 and 7"
+//
+
+function whatday(num) { 
+  const key = {
+    1: 'Sunday',
+    2: 'Monday',
+    3: 'Tuesday',
+    4: 'Wednesday',
+    5: 'Thursday',
+    6: 'Friday',
+    7: 'Saturday', 
+  }
+  return num in key ? key[num] : 'Wrong, please enter a number between 1 and 7'
+}
