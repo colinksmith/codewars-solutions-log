122575192b50a3232987054b98df512bfe1ba068
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-07 14:07:30 -0400

    passed Man in the west 8kyu

--- 7-7-22/daily-kata.js
+++ 7-7-22/daily-kata.js
@@ -424,3 +424,12 @@ function sumMul(n,m){
   }
   return output
 }
+
+//Man in the west
+//
+//A western man is trying to find gold in a river. To do that, he passes a bucket through the river's soil and then checks if it contains any gold. However, he could be more productive if he wrote an algorithm to do the job for him.
+//So, you need to check if there is gold in the bucket, and if so, return True/true. If not, return False/false.
+
+function checkTheBucket(bucket){
+  return bucket.includes('gold')
+}
