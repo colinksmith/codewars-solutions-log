750136bc5edc74931d84f042293cb7ca0e1ec5a3
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-07 17:16:49 -0400

    passed Find out whether the shape is a cube 8kyu

--- 7-7-22/daily-kata.js
+++ 7-7-22/daily-kata.js
@@ -555,3 +555,18 @@ function calculateTip(amount, rating) {
   return 'Rating not recognised'
   }
 }
+
+//Find out whether the shape is a cube
+//
+//To find the volume (centimeters cubed) of a cuboid you use the formula:
+//volume = Length * Width * Height
+//But someone forgot to use proper record keeping, so we only have the volume, and the length of a single side!
+//It's up to you to find out whether the cuboid has equal sides (= is a cube).
+//Return true if the cuboid could have equal sides, return false otherwise.
+//Return false for invalid numbers too (e.g volume or side is less than or equal to 0).
+//Note: the sides must be integers
+
+var cubeChecker = function(volume, side){
+  if (volume < 1 || side < 1) return false
+  return Math.cbrt(volume) === side
+};
