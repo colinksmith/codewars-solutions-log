ab657acffe334df7c6ae26dcd52a8c07c3ea9139
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-08 12:14:12 -0400

    passed Is it a number? 8kyu

--- 7-7-22/daily-kata.js
+++ 7-7-22/daily-kata.js
@@ -635,3 +635,22 @@ function nearestSq(n){
   const lowDiff = Math.abs(n - lowSquare)
   return highDiff < lowDiff ? highSquare : lowSquare
 }
+
+//Is it a number?
+//
+//Given a string s, write a method (function) that will return true if its a valid single integer or floating number or false if its not.
+//Valid examples, should return true:
+//isDigit("3")
+//isDigit("  3  ")
+//isDigit("-3.23")
+//
+//should return false:
+//isDigit("3-4")
+//isDigit("  3   5")
+//isDigit("3 5")
+//isDigit("zero")
+//
+
+function isDigit(s) {
+  return s==parseFloat(s);
+}
