edaff52de00488468388884d46a95470bbc6dd7d
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-09 16:24:40 -0400

    passed Power 8kyu

--- 7-9-22/daily-kata.js
+++ 7-9-22/daily-kata.js
@@ -101,3 +101,24 @@ function uniTotal (string) {
     })
     return sum
   }
+
+//Power
+//
+//The goal is to create a function 'numberToPower(number, power)' that "raises" the number up to power (ie multiplies number by itself power times).
+//Examples
+//numberToPower(3,2)  // -> 9 ( = 3 * 3 )
+//numberToPower(2,3)  // -> 8 ( = 2 * 2 * 2 )
+//numberToPower(10,6) // -> 1000000
+//
+//Note: Math.pow and some other Math functions like eval() and ** are disabled.
+
+function numberToPower(number, power){
+    if (power === 0){
+      return 1
+    }
+    let num = number
+    for(let i = 1; i < power; i++){
+      num *= number
+    }
+    return num
+  }
