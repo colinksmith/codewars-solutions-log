8fca6000ff91ca422e953c5216df2a81585abf9d
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-10 11:18:48 -0400

    passed Multiply the number 8kyu

--- 7-9-22/daily-kata.js
+++ 7-9-22/daily-kata.js
@@ -232,3 +232,37 @@ function grabDoll(dolls){
     }
     return bag;
   }
+
+//Multiply the number
+//
+//Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+//multiply(3)==15 // 3 * 5¹
+//multiply(10)==250 // 10 * 5²
+//multiply(200)==25000 // 200 * 5³
+//multiply(0)==0 // 0 * 5¹
+//multiply(-3)==-15 // -3 * 5¹
+//
+//multiply(3)==15 # 3 * 5¹
+//multiply(10)==250 # 10 * 5²
+//multiply(200)==25000 # 200 * 5³
+//multiply(0)==0 # 0 * 5¹
+//multiply(-3)==-15 # -3 * 5¹
+//
+//multiply(3)==15 # 3 * 5¹
+//multiply(10)==250 # 10 * 5²
+//multiply(200)==25000 # 200 * 5³
+//multiply(0)==0 # 0 * 5¹
+//multiply(-3)==-15 # -3 * 5¹
+//
+//multiply   3  `shouldBe`    15 --  3 * 5¹
+//multiply  10  `shouldBe`   250 --  10 * 5²
+//multiply 200  `shouldBe` 25000 --  200 * 5³
+//multiply   0  `shouldBe`     0 --  0 * 5¹
+//multiply (-3) `shouldBe`  (-15)-- -3 * 5¹
+//
+
+function multiply(number){
+    let numberLength = String(number).length
+    if (number < 0) numberLength -= 1 
+    return number * 5 ** numberLength
+  }
