20eb14823a96802769002458dd0f6fc3a963dbad
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-10 15:37:18 -0400

    passed Object Oriented Piracy 8kyu

--- 7-10-22/daily-kata.js
+++ 7-10-22/daily-kata.js
@@ -202,3 +202,89 @@ function padIt(str,n){
     }
     return output
   }
+
+//Object Oriented Piracy 
+//
+//Ahoy matey!
+//You are a leader of a small pirate crew. And you have a plan.
+//With the help of OOP you wish to make a pretty efficient system to identify ships with a heavy booty on board. 
+//Unfortunattely for you, people weigh a lot this days, so how do you know if a ship if full of gold and not people?
+//You begin with writing a generic Ship class / struct:
+//function Ship(draft,crew) {
+// this.draft = draft;
+// this.crew = crew;
+//}
+//
+//class Ship:
+//    def __init__(self, draft, crew):
+//        self.draft = draft
+//        self.crew = crew
+//
+//public class Ship
+//{
+//  public int Draft;
+//  public int Crew;
+//  
+//  public Ship(int draft, int crew)
+//  {
+//    Draft = draft;
+//    Crew = crew;
+//  }
+//}
+//
+//struct Ship {
+//  draft: u32,
+//  crew: u32,
+//}
+//
+//Every time your spies see a new ship enter the dock, they will create a new ship object based on their observations:
+//
+//draft - an estimate of the ship's weight based on how low it is in the water
+//crew - the count of crew on board
+//
+//var titanic = new Ship(15, 10);
+//
+//Titanic = Ship(15, 10)
+//
+//Ship titanic = new Ship(15, 10);
+//
+//  let titanic = Ship {
+//      draft : 15,
+//      crew : 10,
+//  };
+//
+//Taking into account that an average crew member on board adds 1.5 units to the draft, a ship that has a draft of more than 20 without crew is considered worthy to loot. Any ship weighing that much must have a lot of booty!
+//Add the method
+//isWorthIt
+//
+//is_worth_it
+//
+//is_worth_it(&self)
+//
+//IsWorthIt
+//
+//to decide if the ship is worthy to loot. For example:
+//titanic.isWorthIt() // return false
+//
+//Titanic.is_worth_it()
+//False
+//
+//titanic.IsWorthIt() => false
+//
+//titanic.is_worth_it() -> false
+//
+//This Kata teaches you the very basic of method creation.
+//Good luck!
+
+function Ship(draft,crew) {
+    this.draft = draft;
+    this.crew = crew;
+     
+     this.isWorthIt = function() {
+       if (draft - (crew * 1.5) > 20){
+         return true
+       } else {
+         return false
+       }
+     }
+   }
