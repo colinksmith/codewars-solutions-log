a81178d72dd1d92ab20ad2deeea8eb40721bdf70
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-10 18:30:35 -0400

    passed Classy Classes 8kyu

--- 7-10-22/daily-kata.js
+++ 7-10-22/daily-kata.js
@@ -319,3 +319,20 @@ class God{
 function typeOfSum(a, b) {
     return typeof (a + b)
   }
+  
+
+//Classy Classes
+//
+//Classy Classes
+//Basic Classes, this kata is mainly aimed at the new JS ES6 Update introducing classes
+//Task
+//Your task is to complete this Class, the Person class has been created. You must fill in the Constructor method to accept a name as string and an age as number, complete the get Info property and getInfo method/Info getter which should return johns age is 34
+//Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+
+class Person {
+    constructor(name, age) {
+      this.name = name
+      this.age = age
+      this.info = `${this.name}s age is ${this.age}`
+    }
+  }
