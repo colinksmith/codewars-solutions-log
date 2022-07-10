419e554f8faa828cd023ca053a6f53b19a0bb969
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-10 13:35:51 -0400

    passed Check same case8kyu

--- 7-10-22/daily-kata.js
+++ 7-10-22/daily-kata.js
@@ -33,3 +33,30 @@ function integrate(coefficient, exponent) {
     let returnCoefficient = coefficient / (exponent + 1)
     return `${returnCoefficient}x^${exponent + 1}`
 }
+
+//Check same case
+//
+//Write a function that will check if two given characters are the same case.
+//
+//If either of the characters is not a letter, return -1
+//If both characters are the same case, return 1
+//If both characters are letters, but not the same case, return 0
+//
+//Examples
+//'a' and 'g' returns 1
+//'A' and 'C' returns 1
+//'b' and 'G' returns 0
+//'B' and 'g' returns 0
+//'0' and '?' returns -1
+
+function sameCase(a, b){
+    function isLetter(c) {
+      return c.toLowerCase() != c.toUpperCase();
+    }
+    if(!isLetter(a) || !isLetter(b)) return -1
+    
+    let aUpperCase, bUpperCase
+    a === a.toUpperCase() ? aUpperCase = true : aUpperCase = false
+    b === b.toUpperCase() ? bUpperCase = true : bUpperCase = false
+    return aUpperCase === bUpperCase ? 1 : 0
+  }
