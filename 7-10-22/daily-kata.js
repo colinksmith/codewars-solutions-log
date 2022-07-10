6986ca73759f0a264d7237ceb058a01fc368c4cf
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-10 18:18:48 -0400

    passed Type of sum 8kyu

--- 7-10-22/daily-kata.js
+++ 7-10-22/daily-kata.js
@@ -311,3 +311,11 @@ class God{
     class Woman extends Human{
       
     }
+
+//Type of sum
+//
+//Return the type of the sum of the two arguments
+
+function typeOfSum(a, b) {
+    return typeof (a + b)
+  }
