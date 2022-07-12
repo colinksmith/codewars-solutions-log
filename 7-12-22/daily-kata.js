b483b6ca7d3c36ec30378de8329022d9bc1f7e6b
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-12 07:57:48 -0400

    passed Grader 8kyu

--- 7-12-22/daily-kata.js
+++ 7-12-22/daily-kata.js
@@ -167,3 +167,51 @@ function splitAndMerge(string, separator) {
     })
     return output.join(' ')
   }
+
+//Grader
+//
+//Create a function that takes a number as an argument and returns a grade based on that number.
+//
+//
+//
+//Score
+//Grade
+//
+//
+//
+//Anything greater than 1 or less than 0.6
+//"F"
+//
+//
+//0.9 or greater
+//"A"
+//
+//
+//0.8 or greater
+//"B"
+//
+//
+//0.7 or greater
+//"C"
+//
+//
+//0.6 or greater
+//"D"
+//
+//
+//Examples:
+//grader(0)   should be "F"
+//grader(1.1) should be "F"
+//grader(0.9) should be "A"
+//grader(0.8) should be "B"
+//grader(0.7) should be "C"
+//grader(0.6) should be "D"
+//
+
+function grader(score) {
+    if (score > 1 || score < .6) return 'F'
+    else if (score >= .9) return 'A'
+    else if (score >= .8) return 'B'
+    else if (score >= .7) return 'C'
+    else if (score >= .6) return 'D'
+  }
