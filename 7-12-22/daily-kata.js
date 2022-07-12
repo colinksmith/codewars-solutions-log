27f6cfe3965240fa9e410349a2dd9430f466df83
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-12 08:00:57 -0400

    passed Ensure question 8kyu

--- 7-12-22/daily-kata.js
+++ 7-12-22/daily-kata.js
@@ -215,3 +215,15 @@ function grader(score) {
     else if (score >= .7) return 'C'
     else if (score >= .6) return 'D'
   }
+
+//Ensure question
+//
+//Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+//For example (Input --> Output)
+//"Yes" --> "Yes?" 
+//"No?" --> "No?"
+//
+
+function ensureQuestion(s) {
+    return s[s.length - 1] === '?' ? s : s + '?'
+  }
