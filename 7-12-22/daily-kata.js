9e066d11155de40871b8c2184e0d2941c86489eb
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-12 08:04:17 -0400

    passed Grasshopper - Bug Squashing 8kyu

--- 7-12-22/daily-kata.js
+++ 7-12-22/daily-kata.js
@@ -227,3 +227,51 @@ function grader(score) {
 function ensureQuestion(s) {
     return s[s.length - 1] === '?' ? s : s + '?'
   }
+
+//Grasshopper - Bug Squashing
+//
+//Terminal game bug squashing
+//You are creating a text-based terminal version of your favorite board game. In the board game, each turn has six steps that must happen in this order: roll the dice, move, combat, get coins, buy more health, and print status.
+//
+//You are using a library that already has the functions below. Create a function named main that calls the functions in the proper order.
+//- combat
+//- buyHealth
+//- getCoins
+//- printStatus
+//- rollDice
+//- move
+//
+//- `combat`
+//- `buy_health`
+//- `get_coins`
+//- `print_status`
+//- `roll_dice`
+//- `move`
+//
+//- `combat`
+//- `buy_health`
+//- `get_coins`
+//- `print_status`
+//- `roll_dice`
+//- `move`
+//
+//- `Combat`
+//- `BuyHealth`
+//- `GetCoins`
+//- `PrintStatus`
+//- `RollDice`
+//- `Move`
+//
+
+var health = 100
+var position = 0
+var coins = 0
+
+function main () {
+  rollDice()
+  move()
+  combat()
+  getCoins()  
+  buyHealth()
+  printStatus()
+}
