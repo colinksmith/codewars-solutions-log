33667b021b3a73b557191fb4caa4fbd418e15769
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-14 10:51:05 -0400

    passed Days in the year 8kyu

--- 7-14-22/daily-kata.js
+++ 7-14-22/daily-kata.js
@@ -420,3 +420,24 @@ function periodIsLate(last, today, cycleLength){
   const diffInDays = Math.floor(diffInTime / oneDay)
   return diffInDays > cycleLength ? true : false
 }
+
+//Days in the year
+//
+//A variation of determining leap years, assuming only integers are used and years can be negative and positive.
+//Write a function which will return the days in the year and the year entered in a string.  For example:
+//yearDays(2000) returns "2000 has 366 days"
+//
+//There are a few assumptions we will accept the year 0, even though there is no year 0 in the Gregorian Calendar.
+//Also the basic rule for validating a leap year are as follows
+//Most years that can be divided evenly by 4 are leap years. 
+//Exception: Century years are NOT leap years UNLESS they can be evenly divided by 400.
+//So the years 0, -64 and 2016 will return 366 days.
+//Whilst 1974, -10 and 666 will return 365 days.
+
+function yearDays(year){
+  if (year % 400 === 0) return `${year} has 366 days`
+  if (year % 100 === 0) return `${year} has 365 days`
+  if (year % 4 === 0) return `${year} has 366 days`
+  return `${year} has 365 days`
+
+}
