63ec76dc40831469bfece82ada38a8d67e57c47d
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-17 14:36:44 -0400

    passed Is integer safe to use? 8kyu

--- 07-17-22/daily-kata.js
+++ 07-17-22/daily-kata.js
@@ -23,3 +23,15 @@ function Dog (breed) {
   Dog.prototype.bark = function() {
     return 'Woof'
   }
+
+//Is integer safe to use?
+//
+//Not all integers can be represented by JavaScript/TypeScript. It has space to to represent 53bit signed integers. In this Kata, we've to determine if it is safe to use the integer or not. Make use of the latest ES6 features to find this.
+//SafeInteger(9007199254740990) //true
+//SafeInteger(-90) //true
+//SafeInteger(9007199254740992) //false
+//
+
+function SafeInteger(n) {
+    return n < 9007199254740992 ? true : false
+  }
