124264c5da8e870abb3e88f0c4d8359c6724e15e
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-17 15:23:15 -0400

    passed Did she say hallo? 8kyu

--- 07-17-22/daily-kata.js
+++ 07-17-22/daily-kata.js
@@ -35,3 +35,42 @@ function Dog (breed) {
 function SafeInteger(n) {
     return n < 9007199254740992 ? true : false
   }
+
+//Did she say hallo?
+//
+//You received a whatsup message from an unknown number. Could it be from that girl/boy with a foreign accent you met yesterday evening?
+//Write a simple function to check if the string contains the word hallo in different languages.
+//These are the languages of the possible people you met the night before:
+//
+//hello - english
+//ciao - italian
+//salut - french
+//hallo - german
+//hola - spanish
+//ahoj - czech republic
+//czesc - polish
+//
+//Notes
+//
+//you can assume the input is a string.
+//to keep this a beginner exercise you don't need to check if the greeting is a subset of word (Hallowen can pass the test)
+//function should be case insensitive to pass the tests
+//
+
+function validateHello(greetings) {
+    const helloKey = {
+      hello: 'english',
+      ciao : 'italian',
+      salut : 'french',
+      hallo : 'german',
+      hola : 'spanish',
+      ahoj: 'czech republic',
+      czesc : 'polish',
+    }
+    for (const word in helloKey){
+      if (greetings.toLowerCase().includes(word)){
+        return true
+      }
+    }
+    return false
+  }
