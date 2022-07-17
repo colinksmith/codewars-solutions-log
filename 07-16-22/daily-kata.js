53027a2e949e9f22a22202250126ae4af39526db
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-17 13:54:48 -0400

    passed Training JS #14: Methods of Number object--toString() and toLocaleString() 8kyu

--- 07-16-22/daily-kata.js
+++ 07-16-22/daily-kata.js
@@ -81,3 +81,7 @@
 //#42: Regular Expression--(?:), (?=) and (?!)
 //
 
+function colorOf(r,g,b){
+    return `#${r.toString(16).padStart(2,0)}${g.toString(16).padStart(2,0)}${b.toString(16).padStart(2,0)}`
+  }
+  
