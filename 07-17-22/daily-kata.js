60e92d861052cd9b0c06c4f8f78823e571d8e79e
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-17 16:51:07 -0400

    passed 8kyu interpreters: HQ9+ 8kyu

--- 07-17-22/daily-kata.js
+++ 07-17-22/daily-kata.js
@@ -74,3 +74,71 @@ function validateHello(greetings) {
     }
     return false
   }
+
+//8kyu interpreters: HQ9+
+//
+//You task is to implement an simple interpreter for the notorious esoteric language HQ9+ that will work for a single character input:
+//
+//If the input is 'H', return 'Hello World!'
+//If the input is 'Q', return the input
+//If the input is '9', return the full lyrics of 99 Bottles of Beer. It should be formatted like this:
+//
+//99 bottles of beer on the wall, 99 bottles of beer.
+//Take one down and pass it around, 98 bottles of beer on the wall.
+//98 bottles of beer on the wall, 98 bottles of beer.
+//Take one down and pass it around, 97 bottles of beer on the wall.
+//97 bottles of beer on the wall, 97 bottles of beer.
+//Take one down and pass it around, 96 bottles of beer on the wall.
+//...
+//...
+//...
+//2 bottles of beer on the wall, 2 bottles of beer.
+//Take one down and pass it around, 1 bottle of beer on the wall.
+//1 bottle of beer on the wall, 1 bottle of beer.
+//Take one down and pass it around, no more bottles of beer on the wall.
+//No more bottles of beer on the wall, no more bottles of beer.
+//Go to the store and buy some more, 99 bottles of beer on the wall.
+//
+//
+//For everything else, don't return anything (return null in C#, None in Rust).
+//
+//(+ has no visible effects so we can safely ignore it.)
+
+function HQ9(code) {
+    switch (code) {
+      case 'H':
+        return 'Hello World!'
+        break
+      case 'Q':
+        return code
+        break
+      case '9':
+        return ninetyNine()
+        break
+      default:
+  }
+}
+function ninetyNine() {
+    let lyrics = ''
+    for (let i = 99; i >= 0; i--){
+        switch(i){
+            case 99:
+                lyrics += `${i} bottles of beer on the wall, ${i} bottles of beer.\nTake one down and pass it around, ${i - 1} bottles of beer on the wall.`
+                break
+            case 2:
+                lyrics += `\n2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.`
+                break
+            case 1:
+                lyrics += `\n1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.`
+                break
+            case 0:
+                lyrics += `\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.`
+                break
+            default:
+                lyrics += `\n${i} bottles of beer on the wall, ${i} bottles of beer.\nTake one down and pass it around, ${i - 1} bottles of beer on the wall.`
+                break
+                
+        }
+    }
+    return lyrics
+}
