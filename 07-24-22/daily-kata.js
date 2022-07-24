21bf51d67801b58b973b5bc68ba7b569d2a7a87f
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-24 13:36:57 -0400

    passed Playing with cubes II 8kyu

--- 07-24-22/daily-kata.js
+++ 07-24-22/daily-kata.js
@@ -181,3 +181,24 @@ class Guesser {
 function typeValidation(variable, type) {
     return typeof variable === type ? true : false 
   }
+
+//Playing with cubes II
+//
+//Hey Codewarrior! 
+//You already implemented a Cube class, but now we need your help again! I'm talking about constructors. We don't have one. Let's code two: One taking an integer and one handling no given arguments!
+//Also we got a problem with negative values. Correct the code so negative values will be switched to positive ones!
+//The constructor taking no arguments should assign 0 to Cube's Side property.
+
+// This Cube class needs help
+// Implement the constructor so that it can take an integer for the side or no args
+class Cube {
+    constructor(side = 0){
+      this.side = Math.abs(side)
+    }
+    getSide() {
+      return this.side; 
+    }
+    setSide(n) {
+      this.side = Math.abs(n);
+    }
+  }
