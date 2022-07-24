d052c1aedbb4b08d0668c53c1153d8f3b69e1daf
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-24 13:46:43 -0400

    passed Enumerable Magic #4 - True for None? 8kyu

--- 07-24-22/daily-kata.js
+++ 07-24-22/daily-kata.js
@@ -202,3 +202,13 @@ class Cube {
       this.side = Math.abs(n);
     }
   }
+
+//Enumerable Magic #4 - True for None?
+//
+//Create a method none? (JS none) that accepts an array and a block (JS: a function), and returns true if the block (/function) returns true for none of the items in the array. An empty list should return true.
+//Ruby: If you need help, here is a reference:
+//http://www.rubycuts.com/enum-none
+
+function none(arr, fun){
+    return !arr.some(fun)
+  }
