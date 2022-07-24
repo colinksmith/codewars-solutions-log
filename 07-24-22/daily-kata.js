b63a885797ce11b7adaa01f0ea7b2423c9c8b0cf
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-24 13:10:13 -0400

    passed Finish Guess the Number Game 8kyu

--- 07-24-22/daily-kata.js
+++ 07-24-22/daily-kata.js
@@ -139,3 +139,30 @@ function replaceAll(string, target, replacement){
 function replaceAllOnce(string, target, replacement){
   return string.split(target).join(replacement)
 }
+
+//Finish Guess the Number Game
+//
+//Imagine you are creating a game where the user has to guess the correct number. But there is a limit of how many guesses the user can do.
+//
+//If the user tries to guess more than the limit, the function should throw an error.
+//If the user guess is right it should return true.
+//If the user guess is wrong it should return false and lose a life.
+//
+//Can you finish the game so all the rules are met?
+
+class Guesser {
+    constructor(number, lives) {
+      this.number = number;
+      this.lives = lives;
+    }
+    
+    guess(n) {
+      if (this.lives <= 0) throw 'already dead'
+      if (n === this.number){
+        return true
+      } else {
+        this.lives -= 1 
+        return false
+      }
+    }
+  }
