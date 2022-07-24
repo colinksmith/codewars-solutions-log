8b32fb72e90c1a42015cbe87cb3d2d453fc16ff3
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-24 12:04:21 -0400

    passed Enumerable Magic #2 - True for Any? 8kyu

--- 07-24-22/daily-kata.js
+++ 07-24-22/daily-kata.js
@@ -95,3 +95,13 @@ function whatNumberIsIt(n){
            n === -Infinity ? "Input number is Number.NEGATIVE_INFINITY" :
            `Input number is ${n}`
   }
+
+//Enumerable Magic #2 - True for Any?
+//
+//Create an any? (JS: any) function that accepts an array and a block (JS: function), and returns true if the block (/function) returns true for any item in the array. If the array is empty, the function should return false.
+//Ruby: If you get stuck, you can read up here:
+//http://www.rubycuts.com/enum-any
+
+function any(arr, fun){
+    return arr.some(fun)
+  }
