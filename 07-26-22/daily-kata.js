f1ae1ee77d6f8686b9f200ef3b218519e47e0327
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-26 18:37:27 -0400

    passed this is a problem 8kyu

--- 07-26-22/daily-kata.js
+++ 07-26-22/daily-kata.js
@@ -44,3 +44,43 @@ function playerRankUp (points) {
       return false
     }
   }
+
+//"this" is a problem 
+//
+//We want to create a constructor function 'NameMe', which takes first name and last name as parameters. The function combines the first and last names and saves the value in "name" property.
+//We already implemented that function, but when we actually run the code, the "name" property is accessible, but the "firstName" and "lastName" is not accessible. All the properties should be accessible. Can you find what's wrong with it?
+//A test fixture is also available
+//function NameMe(first, last) {
+//    this.firstName = first;
+//    this.lastName = last;
+//    return {name: this.firstName + ' ' + this.lastName};
+//}
+//
+//var n = new NameMe('John', 'Doe');
+//n.firstName //Expected: John
+//n.lastName //Expected: Doe
+//n.name //Expected: John Doe
+//
+//public class NameMe {
+//    private String firstName;
+//    private String lastName;
+//    private String fullName;
+//
+//    public NameMe(String first, String last) {
+//        this.firstName = first;
+//        this.lastName = last;
+//   }
+// }
+//
+//NameMe nameMe = new NameMe("John", "Doe");
+//nameMe.getFirstName(); //Expected: John
+//nameMe.getLastName(); //Expected: Doe
+//nameMe.getFullName(); //Expected: John Doe
+//
+//
+
+function NameMe(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+    return {name: this.firstName + ' ' + this.lastName, firstName: this.firstName, lastName: this.lastName};
+}
