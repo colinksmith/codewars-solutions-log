8bff78fe02d3cda5fd0758a26e541035d27c1306
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-28 17:33:33 -0400

    passed Circular Objects #1 - Running around in circles 8kyu

--- 07-28-22/daily-kata.js
+++ 07-28-22/daily-kata.js
@@ -607,3 +607,26 @@ Number.prototype.times = function (f) {
 function solution(molarMass1, molarMass2, givenMass1, givenMass2, volume, temp){
     return (((givenMass1 / molarMass1 + givenMass2 / molarMass2)*(0.082)*(temp + 273.15)) / volume)
 }
+
+//Circular Objects #1 - Running around in circles
+//
+//Circular Objects #1 - Running around in circles
+//Overview
+//A circular object is any object containing a property that refers to itself.  An example of a circular object can be found right here at Codewars - the very programming environment that they provide you with.  Just try it - print the details of the current programming environment to the console (I will not be giving you any hints - you'll have to figure this out yourself) and scroll through the printed text until you find a property named global.  As you would expect, the printed output would say that it is [circular].  Now let's try printing the global property of the programming envoronment to the console.  What do you see?  If you followed the instructions properly you would see the exact same thing being printed out!  Impressed?  You can test this further by then printing the global property of the global property of the current environment to the console and you would still see the exact same thing being printed.  In fact, no matter how many levels you go "inside" the object, you would still see the exact same thing being printed out.
+//Which leads us to the task described below ... 
+//Task
+//Define a circular object circular such that the following are true:
+//circular.value === "Hello World"
+//circular === circular.self
+//circular.self === circular.self.self
+//
+//... and so on.
+//NOTE: The code above is pseudocode.  If you actually tested your circular object like this with equality or identity operators, it would not work as object literals do not have an intrinsic value.
+//Articles of Interest
+//Unfortunately, in this Kata, any article I recommend to you would immediately give away the solution so I have decided to not recommend any articles in this Kata of the series.  I apologise for any inconvenience caused.
+
+const circular = {
+    value: "Hello World",
+    get self() {return this}
+  }
+  
