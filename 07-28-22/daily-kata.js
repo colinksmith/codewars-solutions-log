978a6196ea29dde1b2e35032ebda37d3e940cdb3
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-28 17:54:34 -0400

    passed Disemvowel Trolls 7kyu

--- 07-28-22/daily-kata.js
+++ 07-28-22/daily-kata.js
@@ -702,3 +702,18 @@ function getCount(str) {
       else return prev
     },0)
   }
+
+//Disemvowel Trolls
+//
+//Trolls are attacking your comment section!
+//A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+//Your task is to write a function that takes a string and return a new string with all vowels removed.
+//For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+//Note: for this kata y isn't considered a vowel.
+
+function disemvowel(str) {
+    const key = ['a','e','i','o','u']
+    return str.split('').filter((letter) => {
+        return (!(key.includes(letter.toLowerCase())))
+    }).join('')
+  }
