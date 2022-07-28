c5361456498cf1825d0216ea4671a3165b3e9ba8
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-28 12:13:07 -0400

    passed Find variable which breaks strict comparison! 8kyu

--- 07-28-22/daily-kata.js
+++ 07-28-22/daily-kata.js
@@ -57,3 +57,16 @@ function addExtra( listOfNumbers ){
 //There is an object/class already created called MrFreeze.  Mark this object as frozen so that no other changes can be made to it. 
 
 Object.freeze(MrFreeze)
+
+//Find variable which breaks strict comparison!
+//
+//In JavaScript, there is a special case where strict comparison of the same variable returns false! 
+//Try to find out what must be done to get such result!
+//var x = something;
+//x === x // returns false!
+//
+//Write a function which will return value for which strict comparison will give false!
+
+function findStrangeValue() {
+    return NaN
+  }
