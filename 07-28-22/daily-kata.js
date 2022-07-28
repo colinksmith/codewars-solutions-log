f8624056507242f546d8345a95c478d3d7e6d459
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-07-28 16:03:45 -0400

    passed Arguments to Binary addition 8kyu

--- 07-28-22/daily-kata.js
+++ 07-28-22/daily-kata.js
@@ -378,3 +378,36 @@ function magNumber(info){
 //
 
 const a = '0'
+
+//Arguments to Binary addition
+//
+//Given an array add all the number elements and return the binary equivalent of that sum; to make the code bullet proof anything else than a number should be addeded as 0 since it can't be addeded.
+//If your language can handle float binaries assume the array won't contain float or doubles.
+//arr2bin([1,2]) == '11'
+//arr2bin([1,2,'a']) == '11'
+//arr2bin([]) == '0'
+//
+//NOTE: NaN is a number too in javascript for decimal, binary and n-ary base
+//
+//arr2bin [1,2] == '11'
+//arr2bin [1,2,'a'] == '11'
+//arr2bin [] == '0'
+//
+//arr2bin([1,2]) == '11'
+//arr2bin([1,2,'a']) == '11'
+//arr2bin([]) == '0'
+//
+//This is a modification on the Kata: Array2Binary addition hope you like it
+
+function arr2bin(arr){
+    const output = arr.reduce((prev, curr) => {
+      if (typeof curr === 'number'){
+        return prev + curr
+      } else {
+        return prev
+      }
+    }, 0)
+    if (1) {
+      return output.toString(2) 
+      }
+  }
