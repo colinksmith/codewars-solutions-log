0198e3719b0102d31df52431fd6ac21884c90926
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-08-01 21:04:32 -0400

    passed Mumbling 7kyu

--- 08-01-22/daily-kata.js
+++ 08-01-22/daily-kata.js
@@ -12,3 +12,22 @@
 function descendingOrder(n){
     return Number(String(n).split('').sort((a, b) => b - a).join(''))
   }
+
+//Mumbling
+//
+//This time no story, no theory. The examples below show you how to write function accum:
+//Examples:
+//accum("abcd") -> "A-Bb-Ccc-Dddd"
+//accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+//accum("cwAt") -> "C-Ww-Aaa-Tttt"
+//
+//The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+	let output = []
+  s = s.split('').map(x => x.toLowerCase())
+  for (let i = 0; i < s.length; i++){
+    output.push(`${s[i].toUpperCase()}${s[i].repeat(i)}`)
+  }
+  return output.join('-')
+}
