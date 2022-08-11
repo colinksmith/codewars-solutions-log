89aa49017eff072f27638464b4582ebef274e471
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-08-11 18:00:19 -0400

    passed Two to One 7kyu

--- 08-11-22/daily-kata.js
+++ 08-11-22/daily-kata.js
@@ -21,3 +21,21 @@ function getSum( a,b ){
     }
   return output
 }
+
+//Two to One
+//
+//Take 2 strings s1 and s2 including only letters from a to z.
+//Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+//Examples:
+//a = "xyaabbbccccdefww"
+//b = "xxxxyyyyabklmopq"
+//longest(a, b) -> "abcdefklmopqwxy"
+//
+//a = "abcdefghijklmnopqrstuvwxyz"
+//longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+//
+
+function longest(s1, s2) {
+    newS = new Set([...s1, ...s2])
+    return Array.from(newS).sort().join('')
+  }
