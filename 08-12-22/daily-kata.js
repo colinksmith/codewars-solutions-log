b9e7c2cde8e66715116e9418a886825900c0880e
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-08-12 11:27:23 -0400

    passed Find the next perfect square! 7kyu

--- 08-12-22/daily-kata.js
+++ 08-12-22/daily-kata.js
@@ -93,3 +93,20 @@ function openOrSenior(data){
       return x[0] >= 55 && x[1] > 7 ? 'Senior' : 'Open' 
     })
   }
+
+//Find the next perfect square!
+//
+//You might know some pretty large perfect squares. But what about the NEXT one?
+//Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.  
+//If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
+//Examples:(Input --> Output)
+//121 --> 144
+//625 --> 676
+//114 --> -1 since 114 is not a perfect square
+//
+
+function findNextSquare(sq) {
+    let baseNum = Math.sqrt(sq)
+    return baseNum % 1 !== 0 ? -1 :
+    (baseNum + 1) ** 2
+  }
