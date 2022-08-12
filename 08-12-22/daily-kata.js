1023ee902bbc3aab04d9462d7132ea4141689206
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-08-12 15:51:25 -0400

    passed Regex validate PIN code 7kyu

--- 08-12-22/daily-kata.js
+++ 08-12-22/daily-kata.js
@@ -143,4 +143,29 @@ function printerError(s) {
     let errorCount = total - printCount
     return `${errorCount}/${total}`
 }
-console.log(printerError('aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz'))
+
+//Regex validate PIN code
+//
+//ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits. 
+//If the function is passed a valid PIN string, return true, else return false.
+//Examples (Input --> Output)
+//"1234"   -->  true
+//"12345"  -->  false
+//"a234"   -->  false
+//
+
+function validatePIN (pin) {
+    if (pin.includes('.') ||
+        pin.includes('-') ||
+        pin.includes('+')){
+      return false
+    } else if (pin.trim() !== pin){
+      return false    
+    } else if (parseInt(pin) != Number(pin) || pin < 0){
+      return false
+    } else if (!(pin.length === 4 || pin.length === 6)){
+      return false
+    } else {
+      return true
+    }
+  }
