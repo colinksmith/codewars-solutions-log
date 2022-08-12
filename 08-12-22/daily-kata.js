a2c6a6e93d5cde4a7905e3904ce25621f4e9207c
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-08-12 09:48:47 -0400

    passed Friend or Foe? 7kyu

--- 08-12-22/daily-kata.js
+++ 08-12-22/daily-kata.js
@@ -23,3 +23,17 @@ function maskify(cc) {
     return '#'.repeat(minHashes) + last4
   }
   
+
+//Friend or Foe?
+//
+//Make a program that filters a list of strings and returns a list with only your friends name in it.
+//If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+//Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+//i.e.
+//friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+//
+//Note: keep the original order of the names in the output.
+
+function friend(friends){
+    return friends.filter(x => x.length === 4)
+  }
