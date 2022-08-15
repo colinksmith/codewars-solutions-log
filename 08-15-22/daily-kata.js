4999b03297764924b80b8a37491a8412c12a0d30
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-08-15 12:38:08 -0400

    passed String ends with? 7kyu

--- 08-15-22/daily-kata.js
+++ 08-15-22/daily-kata.js
@@ -16,3 +16,15 @@ var number = function(busStops){
     }
     return on - off
   }
+
+//String ends with?
+//
+//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string). 
+//Examples:
+//solution('abc', 'bc') // returns true
+//solution('abc', 'd') // returns false
+//
+
+function solution(str, ending){
+    return ending === str.slice(str.length - ending.length)
+  }
