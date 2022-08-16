0b1f298bdf21afbb5a522558ffe56c31a7abe0c9
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-08-16 12:52:36 -0400

    passed Reverse words 7kyu

--- 08-16-22/daily-kata.js
+++ 08-16-22/daily-kata.js
@@ -22,3 +22,17 @@ function oddOrEven(array) {
     },0)
    return sum % 2 === 0 ? 'even' : 'odd'
  }
+
+//Reverse words
+//
+//Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+//Examples
+//"This is an example!" ==> "sihT si na !elpmaxe"
+//"double  spaces"      ==> "elbuod  secaps"
+//
+
+function reverseWords(str) {
+    return str.split(' ')
+              .map(x => x.split('').reverse().join(''))
+              .join(' ')
+  }
