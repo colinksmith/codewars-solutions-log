862c81bdc0c1d6b54ffed891ce28149033d3c424
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-08-16 10:13:11 -0400

    passed Odd or Even? 7kyu

---
+++ 08-16-22/daily-kata.js
@@ -0,0 +1,24 @@
+//Odd or Even?
+//
+//Task:
+//Given a list of integers, determine whether the sum of its elements is odd or even.
+//Give your answer as a string matching "odd" or "even".
+//If the input array is empty consider it as: [0] (array with a zero).
+//Examples:
+//Input: [0]
+//Output: "even"
+//
+//Input: [0, 1, 4]
+//Output: "odd"
+//
+//Input: [0, -1, -5]
+//Output: "even"
+//
+//Have fun!
+
+function oddOrEven(array) {
+    let sum = array.reduce((prev, curr) => {
+      return prev + curr
+    },0)
+   return sum % 2 === 0 ? 'even' : 'odd'
+ }
