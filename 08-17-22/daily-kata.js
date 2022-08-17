821b2212d5ec7e4e6a2d62464699b8ad4e2ea114
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-08-17 10:28:32 -0400

    passed Testing 1-2-3 7kyu

---
+++ 08-17-22/daily-kata.js
@@ -0,0 +1,17 @@
+//Testing 1-2-3
+//
+//Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+//Write a function which takes a list of strings and returns each line prepended by the correct number.
+//The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+//Examples: (Input --> Output)
+//[] --> []
+//["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+//
+
+var number=function(array){
+    let outputs = []
+    for (let i = 0; i < array.length; i++){
+      outputs.push(`${i+1}: ${array[i]}`)
+    }
+    return outputs
+  }
