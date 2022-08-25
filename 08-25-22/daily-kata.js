802c6e094a8dd7d65995b0754a3a80963ac637da
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-08-25 17:40:43 -0400

    passed Two fighters, one winner. 7kyu

--- 08-25-22/daily-kata.js
+++ 08-25-22/daily-kata.js
@@ -41,3 +41,48 @@ function findSum(n) {
     }
     return output
   }
+
+//Two fighters, one winner.
+//
+//Create a function that returns the name of the winner in a fight between two fighters.
+//Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+//Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+//Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+//Example:
+//  declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
+//  
+//  Lew attacks Harry; Harry now has 3 health.
+//  Harry attacks Lew; Lew now has 6 health.
+//  Lew attacks Harry; Harry now has 1 health.
+//  Harry attacks Lew; Lew now has 2 health.
+//  Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
+//
+//class Fighter(object):
+//    def __init__(self, name, health, damage_per_attack):
+//        self.name = name
+//        self.health = health
+//        self.damage_per_attack = damage_per_attack
+//        
+//    def __str__(self): return "Fighter({}, {}, {})".format(self.name, self.health, self.damage_per_attack)
+//    __repr__=__str__
+//
+function Fighter(name, health, damagePerAttack) {
+    this.name = name;
+    this.health = health;
+    this.damagePerAttack = damagePerAttack;
+    this.toString = function() { return this.name; }
+}
+function declareWinner(fighter1, fighter2, firstAttacker) {
+let attacker = [fighter1,fighter2].find(x => x.name === firstAttacker)
+let defender = [fighter1,fighter2].find(x => x.name !== firstAttacker)
+while (fighter1.health >= 0 && fighter2.health >= 0){
+    defender.health -= attacker.damagePerAttack
+    if (fighter1.health <= 0 || fighter2.health <= 0){
+        break
+    }
+    let tempAttacker = attacker
+    attacker = defender
+    defender = tempAttacker
+    }
+return attacker.name
+}
