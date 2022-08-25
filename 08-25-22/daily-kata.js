c1f0567f3dcf6051278f1baa42d697368233527b
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-08-25 14:20:41 -0400

    passed Sum of all the multiples of 3 or 5 7kyu

--- 08-25-22/daily-kata.js
+++ 08-25-22/daily-kata.js
@@ -23,3 +23,21 @@ function arithmetic(a, b, operator){
         return a / b
     }
   }
+
+//Sum of all the multiples of 3 or 5
+//
+//Your task is to write function findSum.
+//Upto and including n, this function will return the sum of all multiples of 3 and 5.
+//For example:
+//findSum(5) should return 8 (3 + 5)
+//findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+
+function findSum(n) {
+    let output = 0
+    for(let i = 1; i <= n; i++){
+      if (i % 3 === 0 || i % 5 === 0){
+        output += i
+      }
+    }
+    return output
+  }
