e3816dd8ccaca9fb259639dd9d081f64b9c77bf8
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-09-27 12:48:45 -0400

    passed Sum of angles 7kyu

---
+++ 09-27-22/daily-kata.js
@@ -0,0 +1,7 @@
+//Sum of angles
+//
+//Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+function angle(n) {
+    return (n - 2) * 180
+  }
