622764f3c41b739bd8d5be3634d027b4d1ab7977
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-10-24 17:32:17 -0400

    passed Spacify 7kyu

---
+++ 10-24-22/daily-kata.js
@@ -0,0 +1,9 @@
+//Spacify
+//
+//Modify the spacify function so that it returns the given string with spaces inserted between each character.
+//spacify("hello world") // returns "h e l l o   w o r l d"
+//
+
+function spacify(str) {
+    return str.split('').join(' ')
+  }
