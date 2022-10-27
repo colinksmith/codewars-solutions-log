d2e86865259ebb80292d283ada08795d85857034
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-10-27 16:45:11 -0400

    passed Char Code Calculation 7kyu

--- 10-27-22/daily-kata.js
+++ 10-27-22/daily-kata.js
@@ -13,3 +13,36 @@ var filterString = function(value) {
                        .join(''))
   }
   console.log(filterString("a1b2c3"))
+
+//Char Code Calculation
+//
+//Given a string, turn each character into its ASCII character code and join them together to create a number - let's call this number total1:
+//'ABC' --> 'A' = 65, 'B' = 66, 'C' = 67 --> 656667
+//
+//Then replace any incidence of the number 7 with the number 1, and call this number 'total2':
+//total1 = 656667
+//              ^
+//total2 = 656661
+//              ^
+//
+//Then return the difference between the sum of the digits in total1 and total2:
+//  (6 + 5 + 6 + 6 + 6 + 7)
+//- (6 + 5 + 6 + 6 + 6 + 1)
+//-------------------------
+//                       6
+//
+
+function calc(x){
+    let total1 = x.split('').map(x => x.charCodeAt(0)).join('')
+    let total2 = String(total1)
+    while (total2.includes('7')){
+      total2 = total2.replace('7', '1')
+    }
+    return sumDigits(total1) - sumDigits(total2)
+  }
+  function sumDigits(num) {
+    return String(num).split('').reduce((prev, curr) => {
+      return prev + Number(curr)
+    }, 0)
+  }
+  calc('abcdef')
