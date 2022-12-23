6e59ad7112c02104a09376aaa6d512d2605c80e6
Colin Smith <colin.kenjiro.smith@gmail.com>
2022-12-23 13:08:58 -0800

    passed Simple remove duplicates 7kyu

---
+++ 12-23-22/daily-kata.js
@@ -0,0 +1,25 @@
+//Simple remove duplicates
+//
+//Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+//Example:
+//For input: [3, 4, 4, 3, 6, 3]
+//
+//remove the 3 at index 0
+//remove the 4 at index 1
+//remove the 3 at index 3
+//
+//Expected output: [4, 6, 3]
+//More examples can be found in the test cases. 
+//Good luck!
+
+function solve(arr) {
+    let output = []
+    arr.forEach((ele, index, array) => {
+      if (index === array.lastIndexOf(ele)){
+        output.push(ele)
+      }
+    })
+    return output
+  }
+
+  solve([3,4,4,3,6,3]) // [4, 6, 3]
