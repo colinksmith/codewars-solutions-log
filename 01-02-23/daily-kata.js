db29d03228f68fa24cc2b9227a80f3bce4141c6a
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-01-02 17:24:08 -0500

    Currying functions: multiply all elements in an array 7kyu

---
+++ 01-02-23/daily-kata.js
@@ -0,0 +1,15 @@
+//Currying functions: multiply all elements in an array
+//
+//To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of integers as an argument. This function must return another function, which takes a single integer as an argument and returns a new array. 
+//The returned array should consist of each of the elements from the first array multiplied by the integer.
+//Example:
+//multiplyAll([1, 2, 3])(2) = [2, 4, 6];
+//
+//You must not mutate the original array.
+//Here's a nice Youtube video about currying, which might help you if this is new to you.
+
+function multiplyAll(arr){
+    return function mutiply(multiplier) {
+      return arr.map(ele => ele * multiplier)
+    }
+  }
