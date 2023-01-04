7168123b1951047066a3c93aa29fa2a55bfc7d07
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-01-04 11:32:28 -0500

    passed Sort arrays - 1 7kyu

---
+++ 01-04-23/daily-kata.js
@@ -0,0 +1,7 @@
+//Sort arrays - 1
+//
+//Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+sortme = function( names ){
+    return names.sort()
+  }
