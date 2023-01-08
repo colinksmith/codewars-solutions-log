7b1b7f6fced96e3faf8e8140e2d9993ebacd9cd4
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-01-08 17:24:06 -0500

    passed The Office III - Broken Photocopier 7kyu

---
+++ 01-08-23/daily-kata.js
@@ -0,0 +1,15 @@
+//The Office III - Broken Photocopier
+//
+//The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+//Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+//Given a string of binary, return the version the photocopier gives you as a string.
+//The Office I - Outed
+//The Office II - Boredeom Score
+//The Office IV - Find a Meeting Room
+//The Office V - Find a Chair
+
+function broken(x){
+    return x.split('').map(ele => {
+        return ele === '1' ? '0' : '1'
+      }).join('')
+  }
