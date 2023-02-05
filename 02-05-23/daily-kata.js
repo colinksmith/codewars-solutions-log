6c7d56faa40f9880b1776e64003989888558a744
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-02-05 09:24:47 -0500

    passed All Star Code Challenge #22 7kyu

---
+++ 02-05-23/daily-kata.js
@@ -0,0 +1,36 @@
+//All Star Code Challenge #22
+//
+//This Kata is intended as a small challenge for my students
+//All Star Code Challenge #22
+//Create a function that takes an integer argument of seconds and converts the value into a string describing how many hours and minutes comprise that many seconds.
+//Any remaining seconds left over are ignored.
+//Note:The string output needs to be in the specific form - "X hour(s) and X minute(s)"
+//For example:
+//3600 --> "1 hour(s) and 0 minute(s)"
+//3601 --> "1 hour(s) and 0 minute(s)"
+//3500 --> "0 hour(s) and 58 minute(s)"
+//323500 --> "89 hour(s) and 51 minute(s)"
+//
+
+
+function toTime(seconds) {
+    let hours = Math.floor(seconds / 3600)
+    let minutes = Math.floor((seconds % 3600) / 60)
+    return `${hours} hour(s) and ${minutes} minute(s)`
+  }
+  
+
+
+// parameters
+// number of seconds
+
+// returns
+// string like "1 hour(s) and 0 minute(s)"
+
+// examples
+// toTime(3600), '1 hour(s) and 0 minute(s)'
+
+// pseudocode
+// take seconds and / 3600
+//     result floor is hours
+//     set remainder / 60 to minues
