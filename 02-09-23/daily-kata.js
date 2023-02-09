6b1457c54849da71bea62e488259509a888a9137
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-02-09 07:35:54 -0500

    passed Rot13 6kyu

--- 02-09-23/daily-kata.js
+++ 02-09-23/daily-kata.js
@@ -34,3 +34,25 @@ function findOdd(A) {
       }
     }
   }
+
+//Rot13
+//
+//ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+//Create a function that takes a string and returns the string ciphered with Rot13. 
+//If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+
+function rot13(message){
+    const key = 'abcdefghijklmnopqrstuvwxyz'
+    const capKey = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    
+    return message
+        .split('').map( letter => {
+          if (key.includes(letter)){
+            letter = key[(key.indexOf(letter) + 13) % 26]
+          } else if (capKey.includes(letter)){
+            letter = capKey[(capKey.indexOf(letter) + 13) % 26]
+          } 
+          return letter
+        })
+        .join('')
+  }
