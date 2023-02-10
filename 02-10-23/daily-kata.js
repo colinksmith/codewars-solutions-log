5357d4b71e9351dcb77648211cedf9e3cb494e4e
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-02-10 08:04:18 -0500

    passed Who likes it? 6kyu

--- 02-10-23/daily-kata.js
+++ 02-10-23/daily-kata.js
@@ -38,3 +38,47 @@ function solve(a,b){
     
     
     
+
+//Who likes it?
+//
+//You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+//Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+//[]                                -->  "no one likes this"
+//["Peter"]                         -->  "Peter likes this"
+//["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+//["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+//["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+//
+//Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+
+// p: array of strings
+// r: string of who likes this
+// e: ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+// p:
+// switch for how many people like:
+//   case 0:
+//     return 'no one likes this'
+//   case 1:
+//     return `${names[0]} likes this`
+//   case 2:
+//     return `${names[0]} and ${names[1]} like this`
+//   case 3:
+//     return `${names[0]}, ${names[1]} and ${names[2]} like this `
+//   case default:
+//     return `${names[0]}, ${names[1]} and ${names.length - 2} others like this `
+
+function likes(names) {
+    switch (names.length) {
+    case 0:
+      return 'no one likes this'
+    case 1:
+      return `${names[0]} likes this`
+    case 2:
+      return `${names[0]} and ${names[1]} like this`
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    default:
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+    } 
+  }
