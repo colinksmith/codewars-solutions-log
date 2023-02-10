2b46b3b7a26d6b01c9ce11e13f911d4a2ea8bd29
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-02-10 08:41:33 -0500

    passed Maximum subarray sum 5kyu

--- 02-10-23/daily-kata.js
+++ 02-10-23/daily-kata.js
@@ -82,3 +82,47 @@ function likes(names) {
       return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
     } 
   }
+
+//Maximum subarray sum
+//
+//The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
+//maxSequence [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+//-- should be 6: [4, -1, 2, 1]
+//
+//Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
+//Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+
+// p: array of numbers
+// r: number, sum of contiguous subsequence
+// e: maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]) // returns 6
+// p:
+// let maxSum = 0
+// for every number in arr
+//   if number <=0
+//     break
+//   let runningTotal = number
+//   let subIndex = 0
+//   while runningTotal > 0
+    
+//     runningTotal += arr[index + subIndex]
+//     maxSum = Math.max(runningTotal, maxSum)
+// return maxSum
+
+var maxSequence = function(arr){
+    if (arr.length === 0) return 0
+    let maxSum = 0
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] <=0)
+            continue
+        let runningTotal = arr[i]
+        maxSum = Math.max(runningTotal, maxSum)
+        let subIndex = 1
+        while (runningTotal > 0 && i + subIndex < arr.length){
+            runningTotal += arr[i + subIndex]
+            maxSum = Math.max(runningTotal, maxSum)
+            subIndex++
+        }
+    }
+    return maxSum
+}
+  console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
