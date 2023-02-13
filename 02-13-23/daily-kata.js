13673f44bf5687ef3072570cd7100984b23892d8
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-02-13 06:32:13 -0500

    passed Array.diff 6kyu

--- 02-13-23/daily-kata.js
+++ 02-13-23/daily-kata.js
@@ -17,3 +17,23 @@ Array.prototype.remove_ = function(integer_list, values_list){
       }
     })
   }
+
+//Array.diff
+//
+//Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+//It should remove all values from list a, which are present in list b keeping their order.
+//arrayDiff([1,2],[1]) == [2]
+//
+//If a value is present in b, all of its occurrences must be removed from the other:
+//arrayDiff([1,2,2,2,3],[2]) == [1,3]
+//
+
+function arrayDiff(a, b) {
+    return a.filter(ele => {
+    if (b.includes(ele)){
+      return false
+    } else {
+      return true
+    }
+  })
+}
