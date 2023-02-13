a0358ceaa40610cdb22840780b985e76c1799b19
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-02-13 06:30:28 -0500

    passed Remove All The Marked Elements of a List 7kyu

---
+++ 02-13-23/daily-kata.js
@@ -0,0 +1,19 @@
+//Remove All The Marked Elements of a List
+//
+//Define a method/function that removes from a given array of integers all the values contained in a second array.
+//Examples (input -> output):
+//* [1, 1, 2, 3, 1, 2, 3, 4], [1, 3] -> [2, 2, 4]
+//* [1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8], [1, 3, 4, 2] -> [5, 6, 7, 8]
+//* [8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3], [2, 4, 3] -> [8, 7, 6, 5, 1]
+//
+//Enjoy it!!
+
+Array.prototype.remove_ = function(integer_list, values_list){
+    return integer_list.filter(ele => {
+      if (values_list.includes(ele)){
+        return false
+      } else {
+        return true
+      }
+    })
+  }
