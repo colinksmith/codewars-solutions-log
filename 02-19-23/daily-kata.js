aa254bcb6dd4603c85df0d04c26570a7f4c2a853
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-02-18 11:22:11 -0500

    passed Find the lucky numbers 7kyu

---
+++ 02-19-23/daily-kata.js
@@ -0,0 +1,34 @@
+//Responsible Drinking
+//
+//Welcome to the Codewars Bar!
+//Codewars Bar recommends you drink 1 glass of water per standard drink so you're not hungover tomorrow morning.
+//Your fellow coders have bought you several drinks tonight in the form of a string. Return a string suggesting how many glasses of water you should drink to not be hungover.
+//Examples
+//"1 beer"  -->  "1 glass of water"
+//because you drank one standard drink
+//
+//"1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"  -->  "10 glasses of water"
+//because you drank ten standard drinks
+//
+//Note:
+//To keep the things simple, we'll consider that any "numbered thing" in the string is a drink. Even "1 bear" -> "1 glass of water"; or "1 chainsaw and 2 pools" -> "3 glasses of water"...
+
+function hydrate(s) {
+    let numberOfDrinks = s.split(' ')
+      .filter(word => parseInt(word))
+      .reduce((prev, curr) => {
+        return prev + Number(curr)
+      }, 0)
+    let pleural = ''
+    if (numberOfDrinks > 1) pleural = 'es'
+    return `${numberOfDrinks} glass${pleural} of water`
+  }
+  
+  // p: string with num
+  // r: # glass(es) of water
+  // e: "1 shot, 5 beers, 2 shots, 1 glass of wine, 1 beer"  -->  "10 glasses of water"
+  // because you drank ten standard drinks
+  // p:
+  // search string for any numbers
+  // sum numbers
+  // return string of numbers of glasses of water
