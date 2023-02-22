13f3997da2fbea83fa92eed948129160c269ab9b
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-02-22 07:48:04 -0500

    passed Return String of First Characters 7kyu

---
+++ 02-22-23/daily-kata.js
@@ -0,0 +1,11 @@
+//Return String of First Characters
+//
+//In this exercise, a string is passed to a method and a new string has to be returned with the first character of each word in the string.
+//For example:
+//"This Is A Test" ==> "TIAT"
+//
+//Strings will only contain letters and spaces, with exactly 1 space between words, and no leading/trailing spaces.
+
+function makeString(s){
+  return s.split(' ').map(word => word[0]).join('')
+}
