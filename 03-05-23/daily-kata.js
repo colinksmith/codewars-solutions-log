b3351a5e50d407c421e49049d42e28fd7abfcb6f
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-03-05 18:08:01 -0500

    passed Find min and max 7kyu

---
+++ 03-05-23/daily-kata.js
@@ -0,0 +1,7 @@
+//Find min and max
+//
+//Implement a function that returns the minimal and the maximal value of a list (in this order).
+
+function getMinMax(arr){
+    return [Math.min(...arr), Math.max(...arr)]
+  };
