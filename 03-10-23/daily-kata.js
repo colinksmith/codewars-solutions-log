c1e02fa0639af5791a0af59825e822619bb50cf5
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-03-10 09:37:43 -0500

    passed Nickname Generator 7kyu

---
+++ 03-10-23/daily-kata.js
@@ -0,0 +1,33 @@
+//Nickname Generator
+//
+//Nickname Generator
+//Write a function, nicknameGenerator that takes a string name as an argument and returns the first 3 or 4 letters as a nickname.
+//If the 3rd letter is a consonant, return the first 3 letters.
+//nickname("Robert") //=> "Rob"
+//nickname("Kimberly") //=> "Kim"
+//nickname("Samantha") //=> "Sam"
+//
+//If the 3rd letter is a vowel, return the first 4 letters. 
+//nickname("Jeannie") //=> "Jean"
+//nickname("Douglas") //=> "Doug"
+//nickname("Gregory") //=> "Greg"
+//
+//If the string is less than 4 characters, return "Error: Name too short".
+//Notes:
+//
+//Vowels are "aeiou", so discount the letter "y".
+//Input will always be a string.
+//Input will always have the first letter capitalised and the rest lowercase (e.g. Sam).
+//The input can be modified
+//
+
+function nicknameGenerator(name){
+    const vowels = 'aeiou'
+    if (name.length < 4){
+      return 'Error: Name too short'
+    } else if (!vowels.includes(name[2])) {
+      return name.slice(0, 3)
+    } else {
+      return name.slice(0, 4)
+    }
+  }
