e9d70854e8453817ab674d07d7d9a66a8a06357f
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-03-28 14:52:00 -0400

    passed Who is the killer? 7kyu

---
+++ 03-28-23/daily-kata.js
@@ -0,0 +1,25 @@
+//Who is the killer?
+//
+//Who is the killer?
+//Some people have been killed!
+//You have managed to narrow the suspects down to just a few. Luckily, you know every person who those suspects have seen on the day of the murders.
+//Task.
+//Given a dictionary with all the names of the suspects and everyone that they have seen on that day which may look like this:
+//{'James': ['Jacob', 'Bill', 'Lucas'],
+// 'Johnny': ['David', 'Kyle', 'Lucas'],
+// 'Peter': ['Lucy', 'Kyle']}
+//
+//and also a list of the names of the dead people:
+//['Lucas', 'Bill']
+//
+//return the name of the one killer, in our case 'James' because he is the only person that saw both 'Lucas' and 'Bill'
+
+function killer(suspectInfo, dead) {
+    for (let suspect in suspectInfo){
+      if (dead.every(murdered => {
+        return suspectInfo[suspect].includes(murdered)
+      })){
+        return suspect
+      }
+    }
+  }
