5e3fef87ada5d434c7bc27c8da4626af46223b0e
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-04-16 13:22:14 -0400

    passed Javascript filter - 1 7kyu

---
+++ 04-16-23/daily-kata.js
@@ -0,0 +1,18 @@
+//Javascript filter - 1
+//
+//While developing a website, you detect that some of the members have troubles logging in. Searching through the code you find that all logins ending with a "_" make problems. So you want to write a function that takes an array of pairs of login-names and e-mails, and outputs an array of all login-name, e-mails-pairs from the login-names that end with "_".
+//If you have the input-array:
+//[ [ "foo", "foo@foo.com" ], [ "bar_", "bar@bar.com" ] ]
+//
+//it should output
+//[ [ "bar_", "bar@bar.com" ] ]
+//
+//You have to use the filter-method which returns each element of the array for which the filter-method returns true.
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+//
+
+function searchNames( logins ){
+    return logins.filter(arr => {
+      return arr[0][arr[0].length - 1] === '_'
+    })
+  }
