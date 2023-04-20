95e3011a27a2d5d81c610dea7578e4fad795dbbc
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-04-20 15:20:55 -0400

    passed filterEvenLengthWords 7kyu

---
+++ 04-20-23/daily-kata.js
@@ -0,0 +1,10 @@
+//filterEvenLengthWords
+//
+//Write a function called "filterEvenLengthWords".
+//Given an array of strings, "filterEvenLengthWords" returns an array containing only the elements of the given array whose length is an even number.
+//var output = filterEvenLengthWords(['word', 'words', 'word', 'words']);
+//console.log(output); // --> ['word', 'word']
+
+function filterEvenLengthWords(words) {
+    return words.filter(word => word.length % 2 === 0)
+  }
