1d578a333e9b460e415f49017331ebda25ba664a
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-04-23 20:44:51 -0400

    passed Interactive Dictionary 7kyu

---
+++ 04-23-23/daily-kata.js
@@ -0,0 +1,15 @@
+//Interactive Dictionary
+//
+//In this kata, your job is to create a class Dictionary which you can add words to and their entries. Example:
+//>>> d = Dictionary()
+//
+//>>> d.newentry('Apple', 'A fruit that grows on trees')
+//
+//>>> print(d.look('Apple'))
+//A fruit that grows on trees
+//
+//>>> print(d.look('Banana'))
+//Can't find entry for Banana
+//
+//Good luck and happy coding!
+
