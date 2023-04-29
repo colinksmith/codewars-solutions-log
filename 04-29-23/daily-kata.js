b572bc35e66ce60e29669858059cdbef6e4b400f
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-04-29 19:08:32 -0400

    passed Numbers to Objects 7kyu

---
+++ 04-29-23/daily-kata.js
@@ -0,0 +1,17 @@
+//Numbers to Objects
+//
+//You will be given an array of numbers.
+//For each number in the array you will need to create an object. 
+//The object key will be the number, as a string. The value will be the corresponding character code, as a string.
+//Return an array of the resulting objects.
+//All inputs will be arrays of numbers. All character codes are valid lower case letters. The input array will not be empty.
+
+function numObj(s){
+    const output = []
+    for (let i = 0; i < s.length; i++){
+      let obj = {}
+      obj[String(s[i])] = String.fromCharCode(s[i])
+      output.push(obj)
+    }
+    return output
+  }
