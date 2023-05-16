e23b5e1e65717d930ea75699c7ed1f534a733aea
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-05-16 14:07:28 -0400

    passed How many times should I go? 7kyu

---
+++ 05-16-23/daily-kata.js
@@ -0,0 +1,8 @@
+//How many times should I go?
+//
+//Lot of museum allow you to be a member, for a certain amount amount_by_year you can have unlimitted acces to the museum. 
+//In this kata you should complete a function in order to know after how many visit it will be better to take an annual pass. The function take 2 arguments annual_price and individual_price. 
+
+function howManyTimes(annualPrice, individualPrice) {
+    return Math.ceil(annualPrice / individualPrice)
+  }
