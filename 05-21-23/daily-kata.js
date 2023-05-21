979d87a071a1165e904f2fa49503b54e32fffe2a
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-05-21 10:11:03 -0400

    passed Sum a list but ignore any duplicates 7kyu

---
+++ 05-21-23/daily-kata.js
@@ -0,0 +1,14 @@
+//Sum a list but ignore any duplicates
+//
+//Please write a function that sums a list, but ignores any duplicate items in the list.
+//For instance, for the list [3, 4, 3, 6] , the function should return 10.
+
+function sumNoDuplicates(numList) {
+    let sum = 0
+    for (let i = 0; i < numList.length; i++) {
+      if (numList.indexOf(numList[i]) === numList.lastIndexOf(numList[i])){
+        sum += numList[i]
+      }
+    }
+    return sum
+  }
