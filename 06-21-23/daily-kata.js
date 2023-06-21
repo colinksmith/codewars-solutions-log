de72a1978d39a1f32a8207ea4ff31c58197a2e66
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-06-21 14:44:09 -0400

    passed Return substring instance count 7kyu

---
+++ 06-21-23/daily-kata.js
@@ -0,0 +1,11 @@
+//Return substring instance count
+//
+//Complete the solution so that it returns the number of times the search_text is found within the full_text. 
+//Usage example:
+//solution('aa_bb_cc_dd_bb_e', 'bb') # should return 2 since bb shows up twice
+//solution('aaabbbcccc', 'bbb') # should return 1
+//
+
+function solution(fullText, searchText){
+    return fullText.split(searchText).length - 1
+  }
