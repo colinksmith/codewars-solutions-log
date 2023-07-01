e49d9b674527b973e9f4f7d39f4d79ec9f262004
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-07-01 16:21:28 -0400

    passed What a 'Classy' Song

---
+++ 07-01-23/daily-kata.js
@@ -0,0 +1,49 @@
+//What a "Classy" Song
+//
+//Your job is to create a class called Song.
+//A new Song will take two parameters, title and artist.
+//const mountMoose = new Song('Mount Moose', 'The Snazzy Moose');
+//
+//mountMoose.title => 'Mount Moose'
+//mountMoose.artist => 'The Snazzy Moose'
+//
+//You will also have to create an instance method named howMany() (or how_many()).
+//The method takes an array of people who have listened to the song that day. The output should be how many new listeners the song gained on that day out of all listeners. Names should be treated in a case-insensitive manner, i.e. "John" is the same as "john".
+//Example
+//const mountMoose = new Song('Mount Moose', 'The Snazzy Moose');
+//
+//// day 1 (or test 1)
+//mountMoose.howMany(['John', 'Fred', 'BOb', 'carl', 'RyAn']); => 5
+//// These are all new since they are the first listeners.
+//
+//// day 2
+//mountMoose.howMany(['JoHn', 'Luke', 'AmAndA']); => 2
+//// Luke and Amanda are new, john already listened to it the previous day
+//
+//Also if the same person listened to it more than once a day it should only count them once.
+//Example
+//const mountMoose = new Song('Mount Moose', 'The Snazzy Moose');
+//
+//// day 1
+//mountMoose.howMany(['John', 'joHN', 'carl']); => 2
+//
+//Good luck!
+
+class Song {
+    constructor(title, artist) {
+      this.title = title
+      this.artist = artist
+      this.priorListeners = []
+    }
+    howMany(listeners) {
+      let count = 0
+      for (let i = 0; i < listeners.length; i++){
+        listeners[i] = listeners[i].toLowerCase()
+        if (!this.priorListeners.includes(listeners[i])) {
+          count++
+          this.priorListeners.push(listeners[i])
+        }
+      }
+      return count
+    }
+  }
