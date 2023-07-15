02aabe36eee31a7eda62e215a2713c4f0d9b78b5
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-07-15 19:11:07 -0400

    passed Multiples!

---
+++ 07-15-23/daily-kata.js
@@ -0,0 +1,19 @@
+//Multiples!
+//
+//Make a program that takes a value (x) and returns "Bang" if the number is divisible by 3, "Boom" if it is divisible by 5, "BangBoom" if it divisible by 3 and 5, and "Miss" if it isn't divisible by any of them.
+//Note: Your program should only return one value
+//Ex: Input: 105 --> Output: "BangBoom"
+//Ex: Input: 9 --> Output: "Bang"
+//Ex:Input: 25 --> Output: "Boom"
+
+function multiple(x) {
+    if (x % 3 === 0 && x % 5 === 0) {
+      return "BangBoom"
+    } else if (x % 3 === 0) {
+      return "Bang"
+    } else if (x % 5 === 0) {
+      return "Boom"
+    } else {
+      return "Miss"
+    }
+  }
