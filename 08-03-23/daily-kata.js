c1fd14659ceebadc53588318e7940d3b60cf5d41
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-08-03 10:34:19 -0400

    passed Ghostbusters (whitespace removal) 7kyu

---
+++ 08-03-23/daily-kata.js
@@ -0,0 +1,19 @@
+//Ghostbusters (whitespace removal)
+//
+//Oh no! Ghosts have reportedly swarmed the city. It's your job to get rid of them and save the day!
+//In this kata, strings represent buildings while whitespaces within those strings represent ghosts.
+//So what are you waiting for? Return the building(string) without any ghosts(whitespaces)!
+//Example:
+//ghostBusters("Sky scra per");
+//
+//Should return:
+//"Skyscraper"
+//
+//If the building contains no ghosts, return the string:
+//"You just wanted my autograph didn't you?"
+//
+
+function ghostBusters(building) {
+    if (!building.includes(' ')) return "You just wanted my autograph didn't you?"
+    return building.split(' ').join('')
+  }
