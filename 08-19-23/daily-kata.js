71d60195108d835faff9bb2d06b7069ffe4e85a5
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-08-19 10:01:09 -0400

    passed What's my gold score? 7kyu

---
+++ 08-19-23/daily-kata.js
@@ -0,0 +1,18 @@
+//What's my golf score?
+//
+//I have the par value for each hole on a golf course and my stroke score on each hole. I have them stored as strings, because I wrote them down on a sheet of paper.  Right now, I'm using those strings to calculate my golf score by hand: take the difference between my actual score and the par of the hole, and add up the results for all 18 holes.
+//For example:
+//
+//If I took 7 shots on a hole where the par was 5, my score would be: 7 - 5 = 2
+//If I got a hole-in-one where the par was 4, my score would be: 1 - 4 = -3.
+//
+//Doing all this math by hand is really hard! Can you help make my life easier?
+//Task Overview
+//Complete the function which accepts two strings and calculates the golf score of a game. Both strings will be of length 18, and each character in the string will be a number between 1 and 9 inclusive. 
+
+function golfScoreCalculator(parList, scoreList){
+    return sumFromString(scoreList) - sumFromString(parList)
+  }
+  function sumFromString(str) {
+    return str.split('').reduce((prev, curr) => prev + +curr, 0)
+  }
