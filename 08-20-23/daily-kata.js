275086d98f57e85ec4617df1f781dd25a0d14fe9
Colin Smith <colin.kenjiro.smith@gmail.com>
2023-08-20 10:06:23 -0400

    passed 99 Problems, #1 last in list

---
+++ 08-20-23/daily-kata.js
@@ -0,0 +1,11 @@
+//99 Problems, #1: last in list
+//
+//Write a function last that accepts a list and returns the last element in the list.
+//If the list is empty:
+//In languages that have a built-in option or result type (like OCaml or Haskell), return an empty option
+//In languages that do not have an empty option, just return null
+
+const last = xs => {
+    if (xs.length === 0) return null
+    else return xs[xs.length - 1]
+    }
